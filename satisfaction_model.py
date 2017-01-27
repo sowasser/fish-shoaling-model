fb001d2cd66993daa04222d41995e013edd65584
Sophia Wassermann <so.wassermann@gmail.com>
2017-01-27 16:57:40 +0000

    Beginning of a model based on Quera et al. 2016 - dissatisfaction model
    Using the basic code of the Mesa Flocking model and Money Model to implement the method described by Quera et al (2016) in their paper Modelling the emergence of coordinated collective motion by minimizing dissatisfaction. This method will have fewer inputs controlling the behaviour of the agents, allowing polarization and cohesion to become emergent behaviours. In early stages now.

---
+++ satisfaction_model.py
@@ -0,0 +1,73 @@
+"""
+A model following Quera et al (2015) and their model of collective behaviour
+where instead of a boids model with attraction, repulsion, alignment, the model
+operates based on satisfaction/dissatisfaction with a zone, outside of which
+fish are attracted to each other and inside of which fish are repelled from
+each other. Alignment becomes an emergent behaviour.
+
+"""
+import numpy as np
+from mesa import Agent
+
+
+class Fish(Agent):
+    """ Comment """
+    def __init__(self, unique_id, model, pos, speed=5, heading=None,
+                 vision=5, separation=1):
+        """ Create a new Boid flocker agent. Args:
+            unique_id: Unique agent identifyer.
+            pos: Starting position
+            speed: Distance to move per step.
+            heading: numpy vector for the Boid's direction of movement.
+            vision: Radius to look around for nearby Boids.
+            separation: Minimum distance to maintain from other Boids. """
+        super().__init__(unique_id, model)
+        self.pos = pos
+        self.speed = speed
+        if heading is not None:
+            self.heading = heading
+        else:
+            self.heading = np.random.random(2)
+            self.heading /= np.linalg.norm(self.heading)
+        self.vision = vision
+        self.separation = separation
+
+    def cohere(self, neighbors):
+        """ Return the vector toward the center of mass of the local neighbors. """
+        center = np.array([0.0, 0.0])
+        for neighbor in neighbors:
+            center += np.array(neighbor.pos)
+        return center / len(neighbors)
+
+    def separate(self, neighbors):
+        """ Return a vector away from any neighbors closer than separation dist. """
+        my_pos = np.array(self.pos)
+        sep_vector = np.array([0, 0])
+        for neighbor in neighbors:
+            their_pos = np.array(neighbor.pos)
+            dist = np.linalg.norm(my_pos - their_pos)
+            if dist < self.separation:
+                sep_vector -= np.int64(their_pos - my_pos)
+        return sep_vector
+
+    def match_heading(self, neighbors):
+        """ Return a vector of the neighbors' average heading. """
+        mean_heading = np.array([0, 0])
+        for neighbor in neighbors:
+            mean_heading += np.int64(neighbor.heading)
+        return mean_heading / len(neighbors)
+
+    def step(self):
+        """ Get the Boid's neighbors, compute the new vector, and move accordingly."""
+        neighbors = self.model.space.get_neighbors(self.pos, self.vision, False)
+        if len(neighbors) > 0:
+            cohere_vector = self.cohere(neighbors)
+            separate_vector = self.separate(neighbors)
+            match_heading_vector = self.match_heading(neighbors)
+            self.heading += (cohere_vector +
+                             separate_vector +
+                             match_heading_vector)
+            self.heading /= np.linalg.norm(self.heading)
+        new_pos = np.array(self.pos) + self.heading * self.speed
+        new_x, new_y = new_pos
+        self.model.space.move_agent(self, (new_x, new_y))
