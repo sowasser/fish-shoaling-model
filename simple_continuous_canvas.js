e752aa53729e8279180fd112a8afe1e780b105d7
Sophia Wassermann <so.wassermann@gmail.com>
2017-02-28 17:43:45 +0000

    First attempt at adding a triangle icon option
    Failed attempt to add a triangle in addition to circle and rectangle agent icons. Also fixed a typo in "rectangle"

--- simple_continuous_canvas.js
+++ simple_continuous_canvas.js
@@ -1,83 +1,102 @@
 var ContinuousVisualization = function(height, width, context) {
-	var height = height;
-	var width = width;
-	var context = context;
+    var height = height;
+    var width = width;
+    var context = context;
 
-	this.draw = function(objects) {
-		for (var i in objects) {
-			var p = objects[i];
-			if (p.Shape == "rect")
-				this.drawRectange(p.x, p.y, p.w, p.h, p.Color, p.Filled);
-			if (p.Shape == "circle")
-				this.drawCircle(p.x, p.y, p.r, p.Color, p.Filled);
-		};
+    this.draw = function (objects) {
+        for (var i in objects) {
+            var p = objects[i];
+            if (p.Shape == "rect")
+                this.drawRectangle(p.x, p.y, p.w, p.h, p.Color, p.Filled);
+            if (p.Shape == "circle")
+                this.drawCircle(p.x, p.y, p.r, p.Color, p.Filled);
+            if (p.Shape == "triangle")
+                this.drawTriange(p.x, p.y, p.w, p.h.p.Color, p.Filled);
+        }
+        ;
 
-	};
+    };
 
-	this.drawCircle = function(x, y, radius, color, fill) {
-		var cx = x * width;
-		var cy = y * height;
-		var r = radius;
+    this.drawCircle = function (x, y, radius, color, fill) {
+        var cx = x * width;
+        var cy = y * height;
+        var r = radius;
 
-		context.beginPath();
-		context.arc(cx, cy, r, 0, Math.PI * 2, false);
-		context.closePath();
+        context.beginPath();
+        context.arc(cx, cy, r, 0, Math.PI * 2, false);
+        context.closePath();
 
-		context.strokeStyle = color;
-		context.stroke();
+        context.strokeStyle = color;
+        context.stroke();
 
-		if (fill) {
-			context.fillStyle = color;
-			context.fill();
-		}
+        if (fill) {
+            context.fillStyle = color;
+            context.fill();
+        }
 
-	};
+    };
 
-	this.drawRectange = function(x, y, w, h, color, fill) {
-		context.beginPath();
-		var dx = w * width;
-		var dy = h * height;
+    this.drawRectangle = function (x, y, w, h, color, fill) {
+        context.beginPath();
+        var dx = w * width;
+        var dy = h * height;
 
-		// Keep the drawing centered:
-		var x0 = (x*width) - 0.5*dx;
-		var y0 = (y*height) - 0.5*dy;
+        // Keep the drawing centered:
+        var x0 = (x * width) - 0.5 * dx;
+        var y0 = (y * height) - 0.5 * dy;
 
-		context.strokeStyle = color;
-		context.fillStyle = color;
-		if (fill)
-			context.fillRect(x0, y0, dx, dy);
-		else
-			context.strokeRect(x0, y0, dx, dy);
-	};
+        context.strokeStyle = color;
+        context.fillStyle = color;
+        if (fill)
+            context.fillRect(x0, y0, dx, dy);
+        else
+            context.strokeRect(x0, y0, dx, dy);
+    };
 
-	this.resetCanvas = function() {
-		context.clearRect(0, 0, height, width);
-		context.beginPath();
-	};
-};
+    this.drawTriange = function (context, x, y, w, h, color, fill) {
+        context.beginPath();
+        context.moveTo(x, y);
+        context.lineTo(x + w / 2, y + h);
+        context.lineTo(x - w / 2, y + h);
+        context.closePath();
 
-var Simple_Continuous_Module = function(canvas_width, canvas_height) {
-	// Create the element
-	// ------------------
+        context.strokeStyle = color;
+        context.stroke();
 
-	// Create the tag:
-	var canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
-	canvas_tag += "style='border:1px dotted'></canvas>";
-	// Append it to body:
-	var canvas = $(canvas_tag)[0];
-	$("body").append(canvas);
+        if (fill) {
+            context.fillStyle = color;
+            context.fill();
+        }
 
-	// Create the context and the drawing controller:
-	var context = canvas.getContext("2d");
-	var canvasDraw = new ContinuousVisualization(canvas_width, canvas_height, context);
+        this.resetCanvas = function () {
+            context.clearRect(0, 0, height, width);
+            context.beginPath();
+        };
+    };
 
-	this.render = function(data) {
-		canvasDraw.resetCanvas();
-		canvasDraw.draw(data);
-	};
+    var Simple_Continuous_Module = function (canvas_width, canvas_height) {
+        // Create the element
+        // ------------------
 
-	this.reset = function() {
-		canvasDraw.resetCanvas();
-	};
+        // Create the tag:
+        var canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
+        canvas_tag += "style='border:1px dotted'></canvas>";
+        // Append it to body:
+        var canvas = $(canvas_tag)[0];
+        $("body").append(canvas);
 
-};
+        // Create the context and the drawing controller:
+        var context = canvas.getContext("2d");
+        var canvasDraw = new ContinuousVisualization(canvas_width, canvas_height, context);
+
+        this.render = function (data) {
+            canvasDraw.resetCanvas();
+            canvasDraw.draw(data);
+        };
+
+        this.reset = function () {
+            canvasDraw.resetCanvas();
+        };
+
+    };
+}
