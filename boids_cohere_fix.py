44e71c2142db1837972f7029b752c92f8b4fc758
Sophia Wassermann <so.wassermann@gmail.com>
2017-03-30 19:15:59 +0100

    Added a distinction between velocity and heading
    Velocity includes magnitude, heading is normalized and magnitude is 1. This doesn't affect the way the model runs. Also updated documentation.

--- boids_cohere_fix.py
+++ boids_cohere_fix.py
@@ -8,8 +8,8 @@ from mesa.time import RandomActivation
 from mesa.visualization.ModularVisualization import VisualizationElement
 from mesa.visualization.ModularVisualization import ModularServer
 
-# Todo: Constrain the space so that behaviors are more immediately recognizable.
-# Todo: Vision is a positive number - constraining movement to a particular direction
+# Todo: Boids are now all going to the upper-right. Speed seems to be the issue
+# Todo: Constrain the space so that behaviors are more immediately recognizable
 
 
 class Boid(Agent):
@@ -20,50 +20,48 @@ class Boid(Agent):
     define their movement. Avoidance is their desired minimum distance from
     any other Boid.
     """
-    def __init__(self, unique_id, model, pos, speed=3, heading=None,
+    def __init__(self, unique_id, model, pos, speed=3, velocity=None,
                  vision=5, avoidance=1):
         """
         Create a new Boid (bird, fish) agent. Args:
-            unique_id: Unique agent identifier.
-            pos: Starting position
-            heading: randomly generated then transformed into the unit vector
-                     of Boid's movement - no magnitude.
-            velocity: Speed of the Boid, calculated as the Euclidean distance
-                      (vector norm) of the Boid's heading.
+            unique_id: unique agent identifier.
+            pos: starting position
+            speed: distance to move per step. Since it's positive, boids move
+                in a positive direction
+            velocity: randomly generated. Velocity can then be normalized to
+                create the heading - the scalar version w/ no magnitude.
             vision: Radius to look around for nearby Boids.
             avoidance: Minimum distance to maintain from other Boids.
         """
         super().__init__(unique_id, model)
         self.pos = pos
         self.speed = speed
-        if heading is not None:
-            self.heading = heading
+        if velocity is not None:
+            self.velocity = velocity
+            self.heading = self.velocity / np.linalg.norm(self.velocity)
         else:
-            # Does not include the upper number, returns array of 2 values
-            self.heading = np.random.uniform(2) - 0.5
-            self.heading /= np.linalg.norm(self.heading)
+            self.velocity = np.random.uniform(2) - 0.5
+            self.heading /= np.linalg.norm(self.velocity)
         self.vision = vision
         self.avoidance = avoidance
 
     def cohere(self, neighbors):
         """
-        Add the vector (direction, not speed) toward the center of mass of the
-        local neighbors to the position of each agent to return a new vector
-        towards neighbors.
+        Return the vector toward the center of mass of the local neighbors.
         """
         coh_vector = np.array([0.0, 0.0])
         their_pos = [neighbor.pos for neighbor in neighbors]
         their_pos = np.asarray(their_pos)
         center = ndimage.measurements.center_of_mass(their_pos)
         coh_vector += np.subtract(center, self.pos)  # both are tuples
-        if np.linalg.norm(coh_vector) > 0:
+        if np.linalg.norm(coh_vector) > 0:  # when there is already a magnitude
             return coh_vector / np.linalg.norm(coh_vector)
         else:
             return coh_vector
 
     def avoid(self, neighbors):
         """
-        Return a vector away rom any neighbors closer than separation distance.
+        Return a vector away rom any neighbors closer than avoidance distance.
         """
         my_pos = np.array(self.pos)
         avoid_vector = np.array([0, 0])
@@ -77,45 +75,53 @@ class Boid(Agent):
         else:
             return avoid_vector
 
-    def match_heading(self, neighbors):
+    def match_velocity(self, neighbors):
         """
-        Have Boids match the velocity (magnitude/Euclidean distance of heading)
-        of neighbors.
+        Have Boids match the velocity of neighbors.
         """
-        mean_heading = np.array([0.0, 0.0])
+        mean_velocity = np.array([0.0, 0.0])
         for neighbor in neighbors:
-            mean_heading += (neighbor.heading / len(neighbors)) * neighbor.speed
-        return mean_heading
+            mean_velocity += neighbor.velocity / len(neighbors)
+        return mean_velocity
+        # Not checking for an normalizing this function because we want to
+        # retain the magnitude.
 
     def step(self):
-        """ Get the Boid's neighbors, compute the new vector, and move accordingly."""
+        """ 
+        Get the Boid's neighbors, compute the new vector, and move accordingly.
+        """
         neighbors = self.model.space.get_neighbors(self.pos, self.vision, False)
         if len(neighbors) > 0:
             cohere_vector = self.cohere(neighbors)
             avoid_vector = self.avoid(neighbors)
-            match_heading = self.match_heading(neighbors)
+            velocity = self.match_velocity(neighbors)
+            match_heading = velocity / np.linalg.norm(velocity)
             self.heading += (cohere_vector +
                              avoid_vector +
                              match_heading)
-            self.heading /= np.linalg.norm(self.heading)
+        rate = np.linalg.norm(self.heading)
+        self.heading /= rate
         new_pos = np.array(self.pos) + self.heading * self.speed
         new_x, new_y = new_pos
         self.model.space.move_agent(self, (new_x, new_y))
 
 
 class BoidsModel(Model):
-    """ Flocker model class. Handles agent creation, placement and scheduling. """
+    """ 
+    Flocker model class. Handles agent creation, placement and scheduling. 
+    """
 
-    def __init__(self, N, width, height, speed, vision, avoidance):
+    def __init__(self, n, width, height, speed, vision, avoidance):
         """
         Create a new Flockers model. Args:
             N: Number of Boids
             width, height: Size of the space.
-            vision: How far around should each Boid look for its neighbors
-            avoidance: What's the minimum distance each Boid will attempt to
-                       keep from any other
+            speed: how fast the boids should move.
+            vision: how far around should each Boid look for its neighbors
+            avoidance: what's the minimum distance each Boid will attempt to
+                keep from any other
         """
-        self.N = N
+        self.n = n
         self.speed = speed
         self.vision = vision
         self.avoidance = avoidance
@@ -126,13 +132,15 @@ class BoidsModel(Model):
         self.running = True
 
     def make_agents(self):
-        """ Create N agents, with random positions and starting headings. """
-        for i in range(self.N):
+        """ 
+        Create N agents, with random positions and starting velocities. 
+        """
+        for i in range(self.n):
             x = random.random() * self.space.x_max
             y = random.random() * self.space.y_max
             pos = (x, y)
-            heading = np.random.random(2) * 2 - np.array((1, 1))  # Doesn't include upper #, 2d array
-            boid = Boid(unique_id=i, model=self, pos=pos, speed=self.speed, heading=heading,
+            velocity = np.random.random(2) * 2 - np.array((1, 1))  # Doesn't include upper #, 2d array
+            boid = Boid(unique_id=i, model=self, pos=pos, speed=self.speed, velocity=velocity,
                         vision=self.vision, avoidance=self.avoidance)
             self.space.place_agent(boid, pos)
             self.schedule.add(boid)
@@ -178,5 +186,5 @@ def boid_draw(agent):
 
 boid_canvas = SimpleCanvas(boid_draw, 700, 700)
 server = ModularServer(BoidsModel, [boid_canvas], "Boids",
-                       N=100, width=100, height=100, speed=3, vision=5, avoidance=2)
+                       n=100, width=100, height=100, speed=3, vision=5, avoidance=2)
 server.launch()
