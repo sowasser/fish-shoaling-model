6dae1e3876bc79b2494166742d2591fa9f1f0cdb
Sophia Wassermann <so.wassermann@gmail.com>
2017-07-28 14:36:54 +0100

    Working off of the tutorial, no errors but also not showing in notebook

--- data_reading.R
+++ data_reading.R
@@ -1,3 +1,7 @@
 setwd("~/Desktop/Dropbox/Mackerel/Mackerel_Data")
 shoal_data <- read.csv("shoal_data.csv")
 pos_data <- read.csv("position_data.csv")
+
+#find max and min for axes in matplotlib plot
+max(pos_data)
+min(pos_data)
