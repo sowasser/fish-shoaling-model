e3ee46d2df6494da949c291915619b7281a93d5c
Sophia Wassermann <so.wassermann@gmail.com>
2018-01-16 14:29:39 +0000

    Updated scripts to match more modular file structure

--- shoal_model_bounded.py
+++ shoal_model_bounded.py
@@ -23,58 +23,14 @@ This script also includes the code for visualizing the model using an HTML5
 object. The parameters for the visualization rely on a JavaScript canvas.
 """
 
-import numpy as np
-import math
+
 import random
-from scipy.spatial import KDTree
-from statsmodels.robust.scale import mad
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 from continuous_bounded import BoundedSpace
-from mesa.visualization.ModularVisualization import ModularServer
-from mesa.visualization.ModularVisualization import VisualizationElement
-from mesa.visualization.modules import ChartModule
 
-# Todo: Build an arrow-shaped avatar for the agents.
-# Todo: Manipulate agent color in visualization to match degree of cohesion.
-
-
-def polar(model):  # WORKS
-    """
-    Computes median absolute deviation (MAD) from the mean velocity of the
-    group. As the value approaches 0, polarization increases.
-    In order to find the MAD, the x,y coordinates are converted to radians by
-    finding the arc tangent of y/x. The function used pays attention to the
-    sign of the input to make sure that the correct quadrant for the angle is
-    determined.
-    """
-    velocity_x = [agent.velocity[0] for agent in model.schedule.agents]
-    velocity_y = [agent.velocity[1] for agent in model.schedule.agents]
-    angle = []
-    for (y, x) in zip(velocity_y, velocity_x):
-        a = math.atan2(y, x)
-        angle.append(a)
-    return mad(np.asarray(angle), center=np.median)
-
-
-def nnd(model):  # WORKS
-    """
-    Computes the average nearest neighbor distance for each agent as another
-    measure of cohesion. Method finds & averages the nearest neighbours
-    using a KDTree, a machine learning concept for clustering or
-    compartmentalizing data. Right now, the 5 nearest neighbors are considered.
-    """
-    fish = np.asarray([agent.pos for agent in model.schedule.agents])
-    fish_tree = KDTree(fish)
-    means = []
-    for me in fish:
-        neighbors = fish_tree.query(x=me, k=6)  # includes agent @ dist = 0
-        dist = list(neighbors[0])  # select dist not neighbor # from .query output
-        dist.pop(0)  # removes closest agent - itself @ dist = 0
-        mean_dist = sum(dist) / len(dist)
-        means.append(mean_dist)
-    return sum(means) / len(means)
+from data_collectors import *
 
 
 class Fish(Agent):
@@ -236,71 +192,3 @@ class ShoalModel(Model):
     def step(self):
         self.datacollector.collect(self)
         self.schedule.step()
-
-
-###############################################################################
-
-
-# Create canvas for visualization
-class SimpleCanvas(VisualizationElement):
-    """ Uses JavaScript file for a simple, continuous canvas. """
-    local_includes = ["simple_continuous_canvas.js"]
-    portrayal_method = None
-    canvas_height = 500
-    canvas_width = 500
-
-    def __init__(self, portrayal_method, canvas_height=500, canvas_width=500):
-        """ Instantiate a new SimpleCanvas """
-        self.portrayal_method = portrayal_method
-        self.canvas_height = canvas_height
-        self.canvas_width = canvas_width
-        new_element = ("new Simple_Continuous_Module({}, {})".
-                       format(self.canvas_width, self.canvas_height))
-        self.js_code = "elements.push(" + new_element + ");"
-
-    def render(self, model):
-        """ Creates the space in which the agents exist. """
-        space_state = []
-        for obj in model.schedule.agents:
-            portrayal = self.portrayal_method(obj)
-            x, y = obj.pos
-            x = ((x - model.space.x_min) /
-                 (model.space.x_max - model.space.x_min))
-            y = ((y - model.space.y_min) /
-                 (model.space.y_max - model.space.y_min))
-            portrayal["x"] = x
-            portrayal["y"] = y
-            space_state.append(portrayal)
-        return space_state
-
-
-def fish_draw(agent):
-    return {"Shape": "circle", "r": 3, "Filled": "true", "Color": "Blue"}
-    # return {"Shape": "rect", "w": 0.02, "h": 0.02, "Filled": "true", "Color": "Blue"}
-    # return {"Shape": "triangle", "w": 4, "h": 4, "Filled": False, "Color": "Blue", "heading": }
-
-
-# Create canvas, 500x500 pixels
-shoal_canvas = SimpleCanvas(fish_draw, 500, 500)
-
-# Create charts for the data collectors
-polar_chart = ChartModule([{"Label": "Polarization", "Color": "Black"}],
-                          data_collector_name="datacollector",
-                          chart_title="Polarization")
-
-neighbor_chart = ChartModule([{"Label": "Nearest Neighbour Distance",
-                               "Color": "Black"}],
-                             data_collector_name="datacollector",
-                             chart_title="Nearest Neighbour Distance")
-
-
-# Launch server
-server = ModularServer(ShoalModel, [shoal_canvas, polar_chart, neighbor_chart],
-                       "Boids Model of Shoaling Behavior",
-                       population=10,
-                       width=100,
-                       height=100,
-                       speed=1,
-                       vision=10,
-                       separation=2)
-server.launch()
--- shoal_model_pos_data.py
+++ shoal_model_pos_data.py
@@ -18,11 +18,13 @@ import numpy as np
 import pandas as pd
 import itertools
 import random
-from mesa import Agent, Model
+from mesa import Model
 from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 from mesa.space import ContinuousSpace
 import os
+
+from shoal_model import Fish
 # import matplotlib.pyplot as plt
 # from matplotlib import animation
 
@@ -36,86 +38,6 @@ def positions(model):
     pos = [(agent.pos[0], agent.pos[1]) for agent in model.schedule.agents]
     pos = list(itertools.chain(*pos))
     return pos
-
-
-class Fish(Agent):
-    """
-    A Boid-style agent. Boids have a vision that defines the radius in which
-    they look for their neighbors to flock with. Their heading (a unit vector)
-    and their interactions with their neighbors - cohering and avoiding -
-    define their movement. Separation is their desired minimum distance from
-    any other Boid.
-    """
-    def __init__(self, unique_id, model, pos, speed, velocity, vision,
-                 separation, cohere=0.025, separate=0.25, match=0.04):
-        """
-        Create a new Boid (bird, fish) agent.
-        Args:
-            unique_id: Unique agent identifier.
-            pos: Starting position
-            speed: Distance to move per step.
-            velocity: numpy vector for the Boid's direction of movement.
-            vision: Radius to look around for nearby Boids.
-            separation: Minimum distance to maintain from other Boids.
-            cohere: the relative importance of matching neighbors' positions
-            separate: the relative importance of avoiding close neighbors
-            match: the relative importance of matching neighbors' headings
-        """
-        super().__init__(unique_id, model)
-        self.pos = np.array(pos)
-        self.speed = speed
-        self.velocity = velocity
-        self.vision = vision
-        self.separation = separation
-        self.cohere_factor = cohere
-        self.separate_factor = separate
-        self.match_factor = match
-
-    def cohere(self, neighbors):
-        """
-        Return the vector toward the centroid of the local neighbors.
-        """
-        cohere = np.zeros(2)
-        if neighbors:
-            for neighbor in neighbors:
-                cohere += self.model.space.get_heading(self.pos, neighbor.pos)
-            cohere /= len(neighbors)
-        return cohere
-
-    def separate(self, neighbors):
-        """
-        Return a vector away rom any neighbors closer than avoidance distance.
-        """
-        me = self.pos
-        them = (n.pos for n in neighbors)
-        separate_vector = np.zeros(2)
-        for other in them:
-            if self.model.space.get_distance(me, other) < self.separation:
-                separate_vector -= self.model.space.get_heading(me, other)
-        return separate_vector
-
-    def match_velocity(self, neighbors):
-        """
-        Have Boids match the velocity of neighbors.
-        """
-        match_vector = np.zeros(2)
-        if neighbors:
-            for neighbor in neighbors:
-                match_vector += neighbor.velocity
-            match_vector /= len(neighbors)
-        return match_vector
-
-    def step(self):
-        """
-        Get the Boid's neighbors, compute the new vector, and move accordingly.
-        """
-        neighbors = self.model.space.get_neighbors(self.pos, self.vision, False)
-        self.velocity += (self.cohere(neighbors) * self.cohere_factor +
-                          self.separate(neighbors) * self.separate_factor +
-                          self.match_velocity(neighbors) * self.match_factor) / 2
-        self.velocity /= np.linalg.norm(self.velocity)
-        new_pos = self.pos + self.velocity * self.speed
-        self.model.space.move_agent(self, new_pos)
 
 
 class ShoalModel(Model):
