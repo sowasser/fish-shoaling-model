d32c1463372062a60b27144af0cf89fd415eb280
Sophia Wassermann <so.wassermann@gmail.com>
2018-01-19 10:41:10 +0000

    New script for trying to build in a blind spot

---
+++ shoal_model_blindspot.py
@@ -0,0 +1,183 @@
+"""
+Model of shoaling behavior based on the Boids model by Craig Reynolds in 1986,
+using the basic code provided in the Flocker example of the Mesa framework for
+agent-based modelling in Python. This model is based on 3 parameters that each
+agent follows:
+    1. Attraction to other agents (cohere),
+    2. Avoidance of other agents (separation),
+    3. Alignment with other agents (match_velocity).
+
+These rules are based on the neighbours each agent perceives within their
+'vision' radius. This is a geometrical distance, rather than topological
+(Mann et al. 2011). Another version of this model is being constructed
+for the topological (set number of neighbours at any distance).
+
+For this version of the model, the agents have a blind spot behind them. The
+extent of this spot can be manipulated in the model parameters.
+
+Data are collected in the data_collectors.py script and are:
+    1. Polarization: a function returning the median absolute deviation of
+       agent heading from the mean heading of the group
+    2. Nearest neighbour distance: the mean distance of the 5 nearest agents,
+       determined using a k-distance tree.
+    3. Shoal Area: convex hull
+    4. Mean Distance From Centroid
+
+The model is based on an toroidal (unbounded & wrapping), 2D area. Later
+versions will be 3D, bounded and include obstacles, environmental gradients,
+and agents with goal-, food-, or safety-seeking behaviour.
+
+A visualization of the model in an HTML object is in shoal_model_viz.py
+"""
+
+import random
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
+from mesa.space import ContinuousSpace
+
+from data_collectors import *
+
+
+class Fish(Agent):
+    # Todo: add blind spot!
+    """
+    A Boid-style agent. Boids have a vision that defines the radius in which
+    they look for their neighbors to flock with. Their heading (a unit vector)
+    and their interactions with their neighbors - cohering and avoiding -
+    define their movement. Separation is their desired minimum distance from
+    any other Boid.
+    """
+    def __init__(self, unique_id, model, pos, speed, velocity, vision,
+                 separation, cohere=0.025, separate=0.25, match=0.04):
+        """
+        Create a new Boid (bird, fish) agent.
+        Args:
+            unique_id: Unique agent identifier.
+            pos: Starting position
+            speed: Distance to move per step.
+            velocity: numpy vector for the Boid's direction of movement.
+            vision: Radius to look around for nearby Boids.
+            separation: Minimum distance to maintain from other Boids.
+            cohere: the relative importance of matching neighbors' positions
+            separate: the relative importance of avoiding close neighbors
+            match: the relative importance of matching neighbors' headings
+        """
+        super().__init__(unique_id, model)
+        self.pos = np.array(pos)
+        self.speed = speed
+        self.velocity = velocity
+        self.vision = vision
+        self.separation = separation
+        self.cohere_factor = cohere
+        self.separate_factor = separate
+        self.match_factor = match
+
+    def cohere(self, neighbors):
+        """
+        Return the vector toward the centroid of the local neighbors.
+        """
+        cohere = np.zeros(2)
+        if neighbors:
+            for neighbor in neighbors:
+                cohere += self.model.space.get_heading(self.pos, neighbor.pos)
+            cohere /= len(neighbors)
+        return cohere
+
+    def separate(self, neighbors):
+        """
+        Return a vector away rom any neighbors closer than avoidance distance.
+        """
+        me = self.pos
+        them = (n.pos for n in neighbors)
+        separate_vector = np.zeros(2)
+        for other in them:
+            if self.model.space.get_distance(me, other) < self.separation:
+                separate_vector -= self.model.space.get_heading(me, other)
+        return separate_vector
+
+    def match_velocity(self, neighbors):
+        """
+        Have Boids match the velocity of neighbors.
+        """
+        match_vector = np.zeros(2)
+        if neighbors:
+            for neighbor in neighbors:
+                match_vector += neighbor.velocity
+            match_vector /= len(neighbors)
+        return match_vector
+
+    def step(self):
+        """
+        Get the Boid's neighbors, compute the new vector, and move accordingly.
+        """
+        neighbors = self.model.space.get_neighbors(self.pos, self.vision, False)
+        self.velocity += (self.cohere(neighbors) * self.cohere_factor +
+                          self.separate(neighbors) * self.separate_factor +
+                          self.match_velocity(neighbors) * self.match_factor) / 2
+        self.velocity /= np.linalg.norm(self.velocity)
+        new_pos = self.pos + self.velocity * self.speed
+        self.model.space.move_agent(self, new_pos)
+
+
+class ShoalModel(Model):
+    """ Shoal model class. Handles agent creation, placement and scheduling. """
+
+    def __init__(self,
+                 population=100,
+                 width=100,
+                 height=100,
+                 speed=1,
+                 vision=10,
+                 separation=2,
+                 cohere=0.025,
+                 separate=0.25,
+                 match=0.04):
+        # Todo: add parameter for blind spot
+        """
+        Create a new Boids model. Args:
+            N: Number of Boids
+            width, height: Size of the space.
+            speed: how fast the boids should move.
+            vision: how far around should each Boid look for its neighbors
+            separation: what's the minimum distance each Boid will attempt to
+                        keep from any other
+            cohere, separate, match: factors for the relative importance of
+                                     the three drives.
+        """
+        self.population = population
+        self.vision = vision
+        self.speed = speed
+        self.separation = separation
+        self.schedule = RandomActivation(self)
+        self.space = ContinuousSpace(width, height, torus=True,
+                                     grid_width=10, grid_height=10)
+        self.factors = dict(cohere=cohere, separate=separate, match=match)
+        self.make_agents()
+        self.running = True
+
+    def make_agents(self):
+        """
+        Create N agents, with random positions and starting velocities.
+        """
+        for i in range(self.population):
+            x = random.random() * self.space.x_max
+            y = random.random() * self.space.y_max
+            pos = np.array((x, y))
+            velocity = np.random.random(2) * 2 - 1
+            fish = Fish(i, self, pos, self.speed, velocity, self.vision,
+                        self.separation, **self.factors)
+            self.space.place_agent(fish, pos)
+            self.schedule.add(fish)
+
+        self.datacollector = DataCollector(
+            model_reporters={"Polarization": polar,
+                             "Nearest Neighbour Distance": nnd,
+                             "Shoal Area": area,
+                             "Mean Distance from Centroid": centroid_dist})
+
+    def step(self):
+        self.datacollector.collect(self)
+        self.schedule.step()
+
+    # Todo: add slider for blind spot to viz file
