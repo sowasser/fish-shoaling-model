9f053d73525c099d93c001f18b211b41d680a394
Sophia Wassermann <so.wassermann@gmail.com>
2018-02-02 16:38:54 +0000

    Woohooooo! Function written for mean distance from the centroid per step

--- tracking_import_stepwise.py
+++ tracking_import_stepwise.py
@@ -26,46 +26,40 @@ track.columns = ["x1", "y1", "x2", "y2", "x3", "y3", "x4", "y4", "x5", "y5",
 
 # Separate list of tuples for each step, remove empty row to prep for analysis
 s1 = np.asarray(track[track.columns[0:2]].dropna(axis=0))
-s2 = list(track[track.columns[2:4]].dropna(axis=0).itertuples(index=False, name=None))
-s3 = list(track[track.columns[4:6]].dropna(axis=0).itertuples(index=False, name=None))
-s4 = list(track[track.columns[6:8]].dropna(axis=0).itertuples(index=False, name=None))
-s5 = list(track[track.columns[8:10]].dropna(axis=0).itertuples(index=False, name=None))
-s6 = list(track[track.columns[10:12]].dropna(axis=0).itertuples(index=False, name=None))
-s7 = list(track[track.columns[12:14]].dropna(axis=0).itertuples(index=False, name=None))
-s8 = list(track[track.columns[14:16]].dropna(axis=0).itertuples(index=False, name=None))
-s9 = list(track[track.columns[16:18]].dropna(axis=0).itertuples(index=False, name=None))
-s10 = list(track[track.columns[18:20]].dropna(axis=0).itertuples(index=False, name=None))
-s11 = list(track[track.columns[20:22]].dropna(axis=0).itertuples(index=False, name=None))
-s12 = list(track[track.columns[22:24]].dropna(axis=0).itertuples(index=False, name=None))
-s13 = list(track[track.columns[24:26]].dropna(axis=0).itertuples(index=False, name=None))
-s14 = list(track[track.columns[26:28]].dropna(axis=0).itertuples(index=False, name=None))
+s2 = np.asarray(track[track.columns[2:4]].dropna(axis=0))
+s3 = np.asarray(track[track.columns[4:6]].dropna(axis=0))
+s4 = np.asarray(track[track.columns[6:8]].dropna(axis=0))
+s5 = np.asarray(track[track.columns[8:10]].dropna(axis=0))
+s6 = np.asarray(track[track.columns[10:12]].dropna(axis=0))
+s7 = np.asarray(track[track.columns[12:14]].dropna(axis=0))
+s8 = np.asarray(track[track.columns[14:16]].dropna(axis=0))
+s9 = np.asarray(track[track.columns[16:18]].dropna(axis=0))
+s10 = np.asarray(track[track.columns[18:20]].dropna(axis=0))
+s11 = np.asarray(track[track.columns[20:22]].dropna(axis=0))
+s12 = np.asarray(track[track.columns[22:24]].dropna(axis=0))
+s13 = np.asarray(track[track.columns[24:26]].dropna(axis=0))
+s14 = np.asarray(track[track.columns[26:28]].dropna(axis=0))
 
 
 # Distance from centroid for each frame
-
-def distance(df1, df2):
-    """Calculates Euclidean distance between two points."""
-    dist = math.sqrt((df2[:, 0] - df1[:, 0])**2 + (df2[:, 1] - df1[:, 1])**2)
-    return dist
-
-
-def centroid(df):
+def centroid_dist(df):
     """Finds the centroid of each frame."""
     pos_x = df[:, 0]
     pos_y = df[:, 1]
     mean_x, mean_y = np.mean(pos_x), np.mean(pos_y)
-    cent = (mean_x, mean_y)
-    return np.asarray(cent)
+    cent = np.asarray(mean_x, mean_y)
 
+    def distance(array):
+        """Euclidean distance"""
+        dist = np.linalg.norm(array - cent)
+        return dist
 
-def centroid_dist(df):
-    """Calculates mean distance of all fish from the centroid for each frame."""
-    cent_dist = []
-    for i in df:
-        pos = df[i, :]  # issue here
-        dist = distance(pos, centroid(df))
-        cent_dist = np.append(cent_dist, dist)
+    cent_dist = np.apply_along_axis(distance, axis=1, arr=df)
     return np.mean(cent_dist)
 
 
-s1_cd = centroid_dist(s1)
+centroid_distance = [centroid_dist(s1), centroid_dist(s2), centroid_dist(s3),
+                     centroid_dist(s4), centroid_dist(s5), centroid_dist(s6),
+                     centroid_dist(s7), centroid_dist(s8), centroid_dist(s9),
+                     centroid_dist(s10), centroid_dist(s11), centroid_dist(s12),
+                     centroid_dist(s13), centroid_dist(s14)]
