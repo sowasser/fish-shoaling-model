629b82e64cd6256c46f65b2649aa61aa283866f5
Sophia Wassermann <so.wassermann@gmail.com>
2018-04-08 16:00:18 +0100

    Whoops! Forgot distance from centroid

--- data_sensitivity.py
+++ data_sensitivity.py
@@ -18,6 +18,7 @@ or graphed with matplotlib.
 
 import os
 from shoal_model import *
+import matplotlib.pyplot as plt
 
 
 # Data collection for debugging purposes
@@ -36,9 +37,10 @@ model100 = ShoalModel(population=100, width=50, height=50, speed=1, vision=10, s
 for i in range(500):
     model100.step()
 data100 = model100.datacollector.get_model_vars_dataframe()
-nnd100 = data100.columns[0]
-polar100 = data100.columns[1]
-area100 = data100.columns[2]
+cent_dist100 = data100.columns[0]
+nnd100 = data100.columns[1]
+polar100 = data100.columns[2]
+area100 = data100.columns[3]
 # data100.to_csv(os.path.join(path, r"shoal_data_100.csv"), index=",")
 
 # 50 agents
@@ -46,9 +48,10 @@ model50 = ShoalModel(population=50, width=50, height=50, speed=1, vision=10, sep
 for j in range(500):
     model50.step()
 data50 = model50.datacollector.get_model_vars_dataframe()
-nnd50 = data50.columns[0]
-polar50 = data50.columns[1]
-area50 = data50.columns[2]
+cent_dist50 = data100.columns[0]
+nnd50 = data50.columns[1]
+polar50 = data50.columns[2]
+area50 = data50.columns[3]
 # data50.to_csv(os.path.join(path, r"shoal_data_50.csv"), index=",")
 
 # # 200 agents
@@ -56,8 +59,12 @@ model200 = ShoalModel(population=200, width=50, height=50, speed=1, vision=10, s
 for k in range(500):
     model200.step()
 data200 = model200.datacollector.get_model_vars_dataframe()
-nnd200 = data200.columns[0]
-polar200 = data200.columns[1]
-area200 = data200.columns[2]
+cent_dist200 = data200.columns[0]
+nnd200 = data200.columns[1]
+polar200 = data200.columns[2]
+area200 = data200.columns[3]
 # data200.to_csv(os.path.join(path, r"shoal_data_200.csv"), index=",")
+
+
+
 
