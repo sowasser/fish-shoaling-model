2b09df94c0e8145cc9a9ddccc8116e5a3bd3dad3
Sophia Wassermann <so.wassermann@gmail.com>
2018-04-18 16:31:01 +0100

    Well...there's a graph but all 3 are different. Weird.

--- data_sensitivity.py
+++ data_sensitivity.py
@@ -79,66 +79,66 @@ ax3 = plt.subplot(3, 1, 3)
 plt.title("Mean Distance from Centroid (n=200)")
 plt.ylabel("(mm)")
 
-ax1.plot(cent_dist50)
-ax2.plot(cent_dist100)
-ax3.plot(cent_dist200)
+ax1.plot(data50["Mean Distance from Centroid"])
+ax2.plot(data100["Mean Distance from Centroid"])
+ax2.plot(data200["Mean Distance from Centroid"])
 
 plt.tight_layout()
 plt.show()
 
-# Create nearest neighbour distance multiplot
-nnd_fig = plt.figure(figsize=(5, 9), dpi=300)
-ax4 = plt.subplot(3, 1, 1)
-plt.title("Mean Nearest Neighbour Distance (n=50)")
-plt.ylabel("(mm)")
-ax5 = plt.subplot(3, 1, 2)
-plt.title("Mean Nearest Neighbour Distance (n=100)")
-plt.ylabel("(mm)")
-ax6 = plt.subplot(3, 1, 3)
-plt.title("Mean Nearest Neighbour Distance (n=200)")
-plt.ylabel("(mm)")
-
-ax4.plot(nnd50)
-ax5.plot(nnd100)
-ax6.plot(nnd200)
-
-plt.tight_layout()
-plt.show()
-
-# Create polarization multiplot
-polar_fig = plt.figure(figsize=(5, 9), dpi=300)
-ax7 = plt.subplot(3, 1, 1)
-plt.title("Polarization (n=50)")
-ax8 = plt.subplot(3, 1, 2)
-plt.title("Polarization (n=100)")
-ax9 = plt.subplot(3, 1, 3)
-plt.title("Polarization (n=200)")
-
-ax7.plot(polar50)
-ax8.plot(polar100)
-ax9.plot(polar200)
-
-plt.tight_layout()
-plt.show()
-
-# Create shoal area multiplot
-area_fig = plt.figure(figsize=(5, 9), dpi=300)
-ax10 = plt.subplot(3, 1, 1)
-plt.title("Shoal Area (n=50)")
-plt.ylabel("(mm2)")
-ax11 = plt.subplot(3, 1, 2)
-plt.title("Shoal Area (n=100)")
-plt.ylabel("(mm2)")
-ax12 = plt.subplot(3, 1, 3)
-plt.title("Shoal Area (n=200)")
-plt.ylabel("(mm2)")
-
-ax10.plot(area50)
-ax11.plot(area100)
-ax12.plot(area200)
-
-plt.tight_layout()
-plt.show()
+# # Create nearest neighbour distance multiplot
+# nnd_fig = plt.figure(figsize=(5, 9), dpi=300)
+# ax4 = plt.subplot(3, 1, 1)
+# plt.title("Mean Nearest Neighbour Distance (n=50)")
+# plt.ylabel("(mm)")
+# ax5 = plt.subplot(3, 1, 2)
+# plt.title("Mean Nearest Neighbour Distance (n=100)")
+# plt.ylabel("(mm)")
+# ax6 = plt.subplot(3, 1, 3)
+# plt.title("Mean Nearest Neighbour Distance (n=200)")
+# plt.ylabel("(mm)")
+#
+# ax4.plot(nnd50)
+# ax5.plot(nnd100)
+# ax6.plot(nnd200)
+#
+# plt.tight_layout()
+# plt.show()
+#
+# # Create polarization multiplot
+# polar_fig = plt.figure(figsize=(5, 9), dpi=300)
+# ax7 = plt.subplot(3, 1, 1)
+# plt.title("Polarization (n=50)")
+# ax8 = plt.subplot(3, 1, 2)
+# plt.title("Polarization (n=100)")
+# ax9 = plt.subplot(3, 1, 3)
+# plt.title("Polarization (n=200)")
+#
+# ax7.plot(polar50)
+# ax8.plot(polar100)
+# ax9.plot(polar200)
+#
+# plt.tight_layout()
+# plt.show()
+#
+# # Create shoal area multiplot
+# area_fig = plt.figure(figsize=(5, 9), dpi=300)
+# ax10 = plt.subplot(3, 1, 1)
+# plt.title("Shoal Area (n=50)")
+# plt.ylabel("(mm2)")
+# ax11 = plt.subplot(3, 1, 2)
+# plt.title("Shoal Area (n=100)")
+# plt.ylabel("(mm2)")
+# ax12 = plt.subplot(3, 1, 3)
+# plt.title("Shoal Area (n=200)")
+# plt.ylabel("(mm2)")
+#
+# ax10.plot(area50)
+# ax11.plot(area100)
+# ax12.plot(area200)
+#
+# plt.tight_layout()
+# plt.show()
 
 
 # Export figures
