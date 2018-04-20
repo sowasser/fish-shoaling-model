ed6088c14b3dcae0c431f2d12a09f24433b2ebb7
Sophia Wassermann <so.wassermann@gmail.com>
2018-04-20 16:54:14 +0100

    Set up and ready to run & graph for 1000 steps

--- data_sensitivity.py
+++ data_sensitivity.py
@@ -22,36 +22,36 @@ import matplotlib.pyplot as plt
 
 # Data collection for debugging purposes
 # model = ShoalModel(population=100, width=50, height=50, speed=1, vision=10, separation=2)
-# for i in range(100):
+# for i in range(10):
 #     model.step()
-# data1 = model.datacollector.get_model_vars_dataframe()
-
-
-# Collect the data from a single run with x number of steps into a dataframe
-# path = "/Users/user/Desktop/Local/Mackerel/shoal-model-in-R"
-# path_laptop = "/Users/Sophie/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data"
+# data = model.datacollector.get_model_vars_dataframe()
 
 
 # 50 agents
 model50 = ShoalModel(population=50, width=50, height=50, speed=1, vision=10, separation=2)
-for j in range(10):
+for j in range(1000):
     model50.step()
 data50 = model50.datacollector.get_model_vars_dataframe()
 # data50.to_csv(os.path.join(path, r"shoal_data_50.csv"), index=",")
 
 # 100 agents
 model100 = ShoalModel(population=100, width=50, height=50, speed=1, vision=10, separation=2)
-for i in range(10):
+for i in range(1000):
     model100.step()
 data100 = model100.datacollector.get_model_vars_dataframe()
 # data100.to_csv(os.path.join(path, r"shoal_data_100.csv"), index=",")
 
 # # 200 agents
 model200 = ShoalModel(population=200, width=50, height=50, speed=1, vision=10, separation=2)
-for k in range(10):
+for k in range(1000):
     model200.step()
 data200 = model200.datacollector.get_model_vars_dataframe()
 # data200.to_csv(os.path.join(path, r"shoal_data_200.csv"), index=",")
+
+
+# Paths for exporting the data, rather than graphing
+# path = "/Users/user/Desktop/Local/Mackerel/shoal-model-in-R"
+# path_laptop = "/Users/Sophie/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data"
 
 
 # Plotting
