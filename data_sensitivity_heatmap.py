d58ad2e4e3d7624ac6d6b04e1e5756d080972de9
Sophia Wassermann <so.wassermann@gmail.com>
2018-09-25 16:45:28 +0100

    Prep to use the data collectors separately but need better structure

--- data_sensitivity_heatmap.py
+++ data_sensitivity_heatmap.py
@@ -1,14 +1,44 @@
 """
 Code for creating a heatmap of density factors from runs of the shoaling model.
-Data is imported from the original data_sensitivity.py file, then animated
-heatmaps are created in matplotlib.
 """
 
 # Todo: try to create animated heatmaps for density:
 # https://matplotlib.org/examples/images_contours_and_fields/interpolation_methods.html
 # https://stackoverflow.com/questions/33742845/seaborn-animate-heatmap-correlation-matrix?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 
-
-from data_sensitivity import data50, data100, data200
+from shoal_model import *
 import matplotlib.pyplot as plt
+import pandas as pd
 from matplotlib import animation
+
+
+# Collect the data from a single run with x number of steps into a dataframe
+model = ShoalModel(initial_fish=50,
+                   initial_obstruct=4000,
+                   width=100,
+                   height=100,
+                   speed=1,
+                   vision=10,
+                   separation=10)
+for i in range(2):
+    model.step()
+data = model.datacollector.get_model_vars_dataframe()
+
+# Separate data from data collectors into numpy arrays so they can be accessed more easily
+np_data = np.asarray(data)
+polar = np_data[:, 0]
+nnd = np_data[:, 1]
+area = np_data[:, 2]
+centroid_dist = np_data[:, 3]
+mean_pos = np_data[:, 4]
+# Todo: flatten the mean_pos array or otherwise change it to be more simple
+
+print(mean_pos)
+
+# Plotting
+plt.style.use("dark_background")
+fig, ax = plt.subplots()
+ax.scatter(data[0], data[1])
+
+plt.tight_layout()
+plt.show()
