e0bd9dbe43552a42fd9d3b22db5742ac56c29d7a
Sophia Wassermann <so.wassermann@gmail.com>
2018-10-15 14:04:36 +0100

    Added documentation & now selection of which step is while graphing

--- testing.py
+++ testing.py
@@ -11,6 +11,12 @@ import matplotlib.pyplot as plt
 # POSITION DATA MAPPING #
 ###########################################
 
+# This code is from the data_sensitivity_heatmap.py script, so more information
+# and code about how the other data collectors can be pulled out can be found
+# there. Mostly, I'm now trying to figure out how to do animations and hot to
+# get matplotlib's pcolormesh to work for me to create a heatmap of density,
+# rather than a scatter plot.
+
 # Collect the data from a single run with x number of steps into a dataframe
 model = ShoalModel(initial_fish=50,
                    initial_obstruct=4000,
@@ -26,27 +32,24 @@ data = model.datacollector.get_model_vars_dataframe()
 # Separate data from data collectors into numpy arrays so they can be accessed more easily
 np_data = np.asarray(data)
 
-position = np_data[:, 4].flatten()  # remove one set of brackets & make a dataframe
+position = np_data[:, 4].flatten()  # select position data, remove one set of brackets & make a dataframe
 pos_df = pd.DataFrame(position.flatten())  # remove one set of brackets & make a dataframe
 pos_df = pos_df[0].apply(pd.Series)  # remove another set of brackets
 pos_df[0].apply(pd.Series)  # remove last set of brackets
 np_pos = np.asarray(pos_df)  # back to numpy array
 
-# isolate first positions in the first step
-x1 = np_pos[0, 0::2].tolist()
-y1 = np_pos[0, 1::2].tolist()
-step1 = [x1, y1]
+# isolate x and y positions
+x = np_pos[:, 0::2]
+y = np_pos[:, 1::2]
 
 
 # # Plotting
 plt.style.use("dark_background")
 
-x = range(100)
-y = range(100)
 
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111)
 plt.title("Position of Fish")  # Todo: figure out sequential step numbering
 
-scatter = ax.scatter(x1, y1)
+scatter = ax.scatter(x[0, :], y[0, :])  # select which step by selecting a row
 plt.show()
