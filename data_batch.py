6fb41db1ec0f592edef415f60a9f5b19844b3e14
Sophia Wassermann <so.wassermann@gmail.com>
2018-12-11 18:34:45 +0000

    Desperately attempting to run the model many times with data collectors

--- data_batch.py
+++ data_batch.py
@@ -3,9 +3,6 @@ This file is for running multiple iterations of the shoal model under set
 conditions, rather than individual runs for looking at sensitivity, as housed
 in the data_sensitivity.py file.
 
-The Batch Runner allows you to determine which parameters you'd like to keep
-static across the multiple model runs and which you'd like to vary.
-
 Data are collected in the data_collectors.py script and are:
     1. Polarization: a function returning the median absolute deviation of
        agent heading from the mean heading of the group
@@ -16,57 +13,62 @@ Data are collected in the data_collectors.py script and are:
 """
 
 from shoal_model import *
-from mesa.batchrunner import BatchRunner
 import pandas as pd
 import os
 
-path = "/Users/user/Desktop/Local/Mackerel/Mackerel Data"
-
-# Todo: fix batch runner so it works!!!
-# fixed_params = {"initial_fish": 38,
-#                 "initial_obstruct": 4000,
-#                 "width": 100,
-#                 "height": 100}
-#
-# variable_params = {"speed": range(1, 1, 1),
-#                    "vision": range(10, 10, 10),
-#                    "separation": range(2, 2, 2)}
-#
-# batch_run = BatchRunner(ShoalModel,
-#                         fixed_parameters=fixed_params,
-#                         variable_parameters=variable_params,
-#                         iterations=10,  # instantiations of the model
-#                         max_steps=10,  # Run each for 100 steps
-#                         model_reporters={"Polarization": polar,
-#                                          "NND": nnd,
-#                                          "Shoal Area": area,
-#                                          "Mean Distance from Centroid": centroid_dist})
-# batch_run.run_all()
+# path = "/Users/user/Desktop/Local/Mackerel/Mackerel Data"
 #
 #
-# # Data collection methods
-# # Extract data as a DataFrame
-# batch_data = batch_run.get_model_vars_dataframe()
+# model = ShoalModel(initial_fish=38,
+#                    initial_obstruct=4000,
+#                    width=100,
+#                    height=100,
+#                    speed=1,
+#                    vision=10,
+#                    separation=2)
 #
-# batch_data.to_csv(os.path.join(path, r"batch_data.csv"), index=",")
+# for _ in range(10):  # how many steps
+#     model.step()
+# data1 = model.datacollector.get_model_vars_dataframe()
+# data1.to_csv(os.path.join(path, r"data1.csv"))
+#
+# for _ in range(10):
+#     model.step()
+# data2 = model.datacollector.get_model_vars_dataframe()
+# data2.to_csv(os.path.join(path, r"data2.csv"))
+#
+# for _ in range(10):
+#     model.step()
+# data3 = model.datacollector.get_model_vars_dataframe()
+# data3.to_csv(os.path.join(path, r"data3.csv"))
 
 
-model = ShoalModel(initial_fish=38,
-                   initial_obstruct=4000,
-                   width=100,
-                   height=100,
-                   speed=1,
-                   vision=10,
-                   separation=2)
+def mean_model(iterations):
+    polar_all = np.asarray()
+    nnd_all = np.asarray()
+    area_all = np.asarray()
+    cent_all = np.asarray()
+    for i in iterations:
+        model = ShoalModel(initial_fish=38,
+                           initial_obstruct=4000,
+                           width=100,
+                           height=100,
+                           speed=1,
+                           vision=10,
+                           separation=2)
+        for j in range(10):
+            model.step()
+        data = model.datacollector.get_model_vars_dataframe()
+        np_data = data.values
+        polar_run = np_data[:, 1]
+        polar_all = np.append(polar_run)
+        nnd_run = np_data[:, 2]
+        nnd_all = np.append(nnd_run)
+        area_run = np_data[:, 3]
+        area_all = np.append(area_run)
+        cent_run = np_data[:, 4]
+        cent_all = np.append(cent_run)
+    return polar_all, nnd_all, area_all, cent_all
 
-for a in range(10):  # how many steps
-    model.step()
-data1 = model.datacollector.get_model_vars_dataframe()
 
-for b in range(10):
-    model.step()
-data2 = model.datacollector.get_model_vars_dataframe()
-
-for c in range(10):
-    model.step()
-data3 = model.datacollector.get_model_vars_dataframe()
+mean_model(range(2))
