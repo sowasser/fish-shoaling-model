071f017ff4cf6055e488eb46cf0ccff3d6367614
Sophia Wassermann <so.wassermann@gmail.com>
2018-12-12 12:55:04 +0000

    Figured out how to handle data in the loop, now to run model multi times

--- data_batch.py
+++ data_batch.py
@@ -17,37 +17,14 @@ import pandas as pd
 import os
 
 # path = "/Users/user/Desktop/Local/Mackerel/Mackerel Data"
-#
-#
-# model = ShoalModel(initial_fish=38,
-#                    initial_obstruct=4000,
-#                    width=100,
-#                    height=100,
-#                    speed=1,
-#                    vision=10,
-#                    separation=2)
-#
-# for _ in range(10):  # how many steps
-#     model.step()
-# data1 = model.datacollector.get_model_vars_dataframe()
-# data1.to_csv(os.path.join(path, r"data1.csv"))
-#
-# for _ in range(10):
-#     model.step()
-# data2 = model.datacollector.get_model_vars_dataframe()
-# data2.to_csv(os.path.join(path, r"data2.csv"))
-#
-# for _ in range(10):
-#     model.step()
-# data3 = model.datacollector.get_model_vars_dataframe()
-# data3.to_csv(os.path.join(path, r"data3.csv"))
 
 
 def mean_model(iterations):
-    polar_all = np.asarray()
-    nnd_all = np.asarray()
-    area_all = np.asarray()
-    cent_all = np.asarray()
+    """
+    Runs the model many times and takes the average of all data collectors for
+    all runs for each step of the model.
+    """
+    steps = pd.DataFrame()
     for i in iterations:
         model = ShoalModel(initial_fish=38,
                            initial_obstruct=4000,
@@ -59,16 +36,16 @@ def mean_model(iterations):
         for j in range(10):
             model.step()
         data = model.datacollector.get_model_vars_dataframe()
-        np_data = data.values
-        polar_run = np_data[:, 1]
-        polar_all = np.append(polar_run)
-        nnd_run = np_data[:, 2]
-        nnd_all = np.append(nnd_run)
-        area_run = np_data[:, 3]
-        area_all = np.append(area_run)
-        cent_run = np_data[:, 4]
-        cent_all = np.append(cent_run)
-    return polar_all, nnd_all, area_all, cent_all
+        polar_run = data.iloc[:, 0]
+        polar_all = steps.append(polar_run, ignore_index=True)
+        # nnd_run = data.iloc[:, 1]
+        # nnd_all = steps.append(nnd_run, ignore_index=True)
+        # area_run = data.iloc[:, 2]
+        # area_all = steps.append(area_run, ignore_index=True)
+        # cent_run = data.iloc[:, 3]
+        # cent_all = steps.append(cent_run, ignore_index=True)
+        # return polar_all, nnd_all, area_all, cent_all
+    return polar_all
 
 
 mean_model(range(2))
