00cd4ec774d9b0d64f35650fca880701f3f7f05c
Sophia Wassermann <so.wassermann@gmail.com>
2018-12-13 09:58:37 +0000

    Improved documentation

--- data_batch.py
+++ data_batch.py
@@ -3,6 +3,17 @@ This file is for running multiple iterations of the shoal model under set
 conditions, rather than individual runs for looking at sensitivity, as housed
 in the data_sensitivity.py file.
 
+The Mesa batch runner allows you to change some of the variables and run
+the model multiple times, but it's only capable of returning a single data
+point, not one per step, like with the data collectors when the model is run
+once.
+
+In this code, the model is run within a function that can be called as many
+times as needed...manually, unfortunately. Then the means are taken across all
+of the model runs, for each step of the model.
+
+# Todo: find some way to avoid doing this manually!
+
 Data are collected in the data_collectors.py script and are:
     1. Polarization: a function returning the median absolute deviation of
        agent heading from the mean heading of the group
@@ -21,7 +32,8 @@ import os
 
 def run_model(steps):
     """
-    Runs the shoal model for a certain number of steps and pulls out polarization.
+    Runs the shoal model for a certain number of steps, returning a dataframe
+    with all of the data collectors.
     """
     model = ShoalModel(initial_fish=38,
                        initial_obstruct=4000,
@@ -36,14 +48,18 @@ def run_model(steps):
     return data
 
 
+# Isolate the polarization data from many runs of the model
 p = pd.DataFrame([list(run_model(10).iloc[:, 0]), list(run_model(10).iloc[:, 0])])
 polar_mean = p.mean(axis=0)
 
+# Isolate the nearest neighbour distance data from many runs of the model
 n = pd.DataFrame([list(run_model(10).iloc[:, 1]), list(run_model(10).iloc[:, 1])])
 nnd_mean = n.mean(axis=0)
 
+# Isolate the shoal area data from many runs of the model
 a = pd.DataFrame([list(run_model(10).iloc[:, 2]), list(run_model(10).iloc[:, 2])])
 area_mean = a.mean(axis=0)
 
+# Isolate the mean distance from the centroid data from many runs of the model
 c = pd.DataFrame([list(run_model(10).iloc[:, 3]), list(run_model(10).iloc[:, 3])])
 cent_mean = c.mean(axis=0)
