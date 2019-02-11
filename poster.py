ded602ad3d4a77d1bbaca4b28c7e2c8435025b01
Sophia Wassermann <so.wassermann@gmail.com>
2019-02-11 18:30:21 +0000

    This was just practice for my poster styled after a python script

--- poster.py
+++
@@ -1,7 +0,0 @@
-def its_wrong_but_is_it_useful(var):
-    """
-    Validating theoretical models of fish collective behaviour.
-    """
-    authors = ["Sophia N. Wassermann", "Mark P. Johnson"]
-    affiliation = "Ryan Institute, National University of Ireland, Galway"
-    return authors, affiliation
