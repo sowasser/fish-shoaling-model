3065504bcedfb5694d80e1f4bb160a3e000a4f59
Sophia Wassermann <so.wassermann@gmail.com>
2019-09-04 11:58:25 +0100

    Now this script is more automated & only gives mean for each step
    Adapted from the sensitivity script & excluded means for each run cause that's what the sensitivity script provides.

--- data_handling/data_batch.py
+++ data_handling/data_batch.py
@@ -9,10 +9,9 @@ point, not one per step, like with the data collectors when the model is run
 once.
 
 In this code, the model is run within a function that can be called as many
-times as needed...manually, unfortunately. The different data collectors Then the means are taken across all
+times as needed. Then the means of each data collector are taken across all
 of the model runs, for each step of the model.
 
-# Todo: find some way to avoid doing this manually!
 # Todo: find if there's some consistent amount of burn-in for shoal formation
 
 Data are collected in the data_collectors.py script and are:
@@ -27,10 +26,12 @@ Data are collected in the data_collectors.py script and are:
 from shoal_model import *
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
 
-path = "/Users/user/Desktop/Local/Mackerel/Mackerel Data"
+path = "/Users/user/Desktop/Local/Mackerel/Mackerel Data"  # for desktop
 # path = "/Users/Sophie/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data"  # for laptop
+
+s = 200  # number of steps to run the model for each time
+runs = range(100)  # number of runs/iterations of the model for finding the mean
 
 
 def run_model(steps):
@@ -38,64 +39,29 @@ def run_model(steps):
     Runs the shoal model for a certain number of steps, returning a dataframe
     with all of the data collectors.
     """
-    model = ShoalModel()
-    for j in range(steps):
-        model.step()
-    data = model.datacollector.get_model_vars_dataframe()
-    return data
+    model = ShoalModel(n_fish=50,
+                       width=50,
+                       height=50,
+                       speed=2,
+                       vision=10,
+                       separation=2)
+    for step in range(steps):
+        model.step()  # run the model for certain number of steps
+    data = model.datacollector.get_model_vars_dataframe()  # retrieve data from model
+    return data.T  # data transposed to make means calculation easier
 
 
-s = 100  # number of steps to run the model for each time
-r = 100  # number of runs of the model
+# RUN MODELS MANY TIMES, FIND MEAN FOR EACH STEP, EXPORT ----------------------
 
+polar = pd.concat([run_model(s).iloc[0:1, ] for r in runs])
+nnd = pd.concat([run_model(s).iloc[1:2, ] for r in runs])
+area = pd.concat([run_model(s).iloc[2:3, ] for r in runs])
+cent = pd.concat([run_model(s).iloc[3:4, ] for r in runs])
 
-# Isolate the polarization data from many runs of the model
-p = pd.DataFrame()
-for run in range(r):
-    p = p.append(run_model(s).iloc[:, 0])  # add each run to data frame
-p = p.T  # transpose dataframe so each column is a run & each row is a step
-# p.to_csv(os.path.join(path, r"polar_batch.csv"))  # save data to use in R
+step_means = pd.concat([polar.mean(axis=0),
+                        nnd.mean(axis=0),
+                        area.mean(axis=0),
+                        cent.mean(axis=0)], axis=1)
+step_means.columns = ["polar", "nnd", "area", "centroid"]
 
-
-# Isolate the nearest neighbour distance data from many runs of the model
-n = pd.DataFrame()
-for run in range(r):
-    n = n.append(run_model(s).iloc[:, 1])
-n = n.T
-# n.to_csv(os.path.join(path, r"nnd_batch.csv"))
-
-
-# Isolate the shoal area data from many runs of the model
-a = pd.DataFrame()
-for run in range(r):
-    a = a.append(run_model(s).iloc[:, 2])
-a = a.T
-# a.to_csv(os.path.join(path, r"area_batch.csv"))
-
-# Isolate the mean distance from the centroid data from many runs of the model
-c = pd.DataFrame()
-for run in range(r):
-    c = c.append(run_model(s).iloc[:, 3])
-c = c.T
-# c.to_csv(os.path.join(path, r"cent_batch.csv"))
-
-
-# CALCULATE MEANS -------------------------------------------------------------
-
-# For each step over all runs
-mean_runs = pd.concat([p.mean(axis=1), n.mean(axis=1), a.mean(axis=1), c.mean(axis=1)], axis=1)
-mean_runs.columns = ["polar", "nnd", "area", "centroid"]
-# mean_runs.to_csv(os.path.join(path, r"batch_means_runs.csv"))
-
-
-# For each run over all steps
-mean_steps = pd.concat([p.mean(axis=0).reset_index(drop=True),
-                        n.mean(axis=0).reset_index(drop=True),
-                        a.mean(axis=0).reset_index(drop=True),
-                        c.mean(axis=0).reset_index(drop=True)], axis=1)
-mean_steps.columns = ["polar", "nnd", "area", "centroid"]
-# mean_steps.to_csv(os.path.join(path, r"batch_means_steps.csv"))
-
-# For all steps and all runs
-overall_mean = pd.DataFrame(mean_runs.mean(axis=0))
-# overall_mean.to_csv(os.path.join(path, r"overall_means.csv"))
+step_means.to_csv(os.path.join(path, "step_means.csv"))
