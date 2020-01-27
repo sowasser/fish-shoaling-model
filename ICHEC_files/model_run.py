11b0c7bdb2aa0fc985ce0034fe4bddb613f27f33
Sophia Wassermann <so.wassermann@gmail.com>
2020-01-27 16:42:48 +0000

    Added shorter column names - don't need special print change now

--- ICHEC_files/model_run.py
+++ ICHEC_files/model_run.py
@@ -7,16 +7,12 @@ Basic script for running the model on the Irish High End Computing Cluster
 # Todo: figure out how to combine data afterwards
 
 from shoal_model import *
-import pandas as pd
 
 # Collect the data from a single run with x number of steps into a dataframe
 model = ShoalModel()
 for i in range(10):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
-# data.columns = ["polar", "nnd", "area", "centroid", "mass"]
-
-# print all columns so they output on the cluster
-pd.set_option("display.max_columns", None)
+data.columns = ["polar", "nnd", "area", "cent"]
 
 print(data)
