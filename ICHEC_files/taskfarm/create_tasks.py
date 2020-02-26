c663eaa186517a66f4869c170ad4e616f8b7ba7c
Sophia Wassermann <hi@sowasser.com>
2020-02-26 19:09:02 +0000

    create_tasks.py now writes commands using the 3 different ICHEC scripts

--- ICHEC_files/taskfarm/create_tasks.py
+++ ICHEC_files/taskfarm/create_tasks.py
@@ -2,15 +2,25 @@
 Script for generating the task list to run the taskfarm on the ICHEC cluster.
 The tasks are run based on a .txt file where each run is a task that has its
 own data output.
-
-TODO: fiure out how to break up the different parameters.
 """
 
 import os
 
 path = "/Users/user/Desktop/Local/Mackerel/fish-shoaling-model/ICHEC_files/taskfarm"
 file = open(os.path.join(path, r"modelruns.txt"), "w")
-for i in range(10):
-    file.write("python3 ../../ichec_run.py > ../output/25feb2020/task" + str(i) + "_output.txt \n")
+
+# Write a line running the scripts varying each parameter (speed, vision,
+# separation) i times
+for i in range(3):
+    file.write("python3 ../../ichec_run_speed.py > ../output/26feb2020/speed_output"
+               + str(i) + ".txt \n")
+
+for i in range(3):
+    file.write("python3 ../../ichec_run_vision.py > ../output/26feb2020/vision_output"
+               + str(i) + ".txt \n")
+
+for i in range(3):
+    file.write("python3 ../../ichec_run_sep.py > ../output/26feb2020/sep_output"
+               + str(i) + ".txt \n")
 
 file.close()
