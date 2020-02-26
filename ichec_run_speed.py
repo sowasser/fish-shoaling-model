854b2cb68c2b5742de89e3d773b63ea95fc65ebf
Sophia Wassermann <hi@sowasser.com>
2020-02-26 18:56:34 +0000

    Created new ICHEC model run scripts for each of the parameters I vary
    These scripts are based on the data_sensitivity.py script that I run locally. The difference here is that they're broken up into the different types so I can run each a set number of times.
    
    The question is - what's the balance between the number of model runs inside each script and how many times I call the script on the taskfarm on the cluster? That seems like a bit of a tricky question to figure out, beyond trial and error. 
    
    Also, hopefully, the data output will be as simple as it was for the test scripts, but somehow I doubt that will be the case....

---
+++ ichec_run_speed.py
@@ -0,0 +1,59 @@
+"""
+This file is for testing the effect of different parameter values on the output
+from the data collectors, when running on the ICHEC cluster. In this script,
+speed varies while vision and separation remain fixed.
+"""
+
+from shoal_model import *
+import pandas as pd
+from scipy.stats import gamma
+
+
+# SET PARAMETER & MODEL VALUES ------------------------------------------------
+
+# Fixed values for non-tested parameters
+vision_fixed = 10
+sep_fixed = 2
+
+# Todo: figure out how to get the range we want. Must be non-negative!
+
+# Defines the distribution as a range of values. Size is # of variables (and
+# therefore runs of the model), a is the number that the distribution is based
+# around.
+speed_dist = gamma.rvs(size=100, a=2)
+
+steps = 200  # number of steps to run the model for each time
+
+burn_in = 10  # number of steps to exclude at the beginning as collective behaviour emerges
+
+
+# RUN MODELS & COLLECT DATA ---------------------------------------------------
+def run_speed_model(speed):
+    """
+    Runs the shoal model for a certain number of steps with agent speed varying
+    while all other parameters are fixed. Returns a dataframe with the average
+    per run of all data collectors (average of all steps) and columns with the
+    parameter values for that run, including the varying & fixed parameters so
+    all dataframes can be stacked together.
+    """
+    speed_parameter = []
+    model = ShoalModel(n_fish=20,
+                       width=50,
+                       height=50,
+                       speed=speed_dist,
+                       vision=vision_fixed,
+                       separation=sep_fixed)
+    for step in range(steps):
+        model.step()  # run the model for certain number of steps
+        speed_parameter.append(speed)  # create list of parameter values tested
+    data = model.datacollector.get_model_vars_dataframe()  # retrieve data from model
+    data["speed"] = speed_parameter  # add parameter value column
+    data["vision"] = vision_fixed  # add vision column
+    data["separation"] = sep_fixed  # add separation column
+    data_trim = data.iloc[burn_in:, ]  # remove early runs
+    return pd.DataFrame(data_trim.mean(axis=0)).T
+
+
+# Run the model as many times as there are parameter values
+speed_data = pd.concat([run_speed_model(i) for i in speed_dist])
+print(speed_data)  # printing makes the data accessible from the cluster.
---
+++ ichec_run_vision.py
@@ -0,0 +1,59 @@
+"""
+This file is for testing the effect of different parameter values on the output
+from the data collectors, when running on the ICHEC cluster. In this script,
+vision varies while speed and separation remain fixed.
+"""
+
+from shoal_model import *
+import pandas as pd
+from scipy.stats import gamma
+
+
+# SET PARAMETER & MODEL VALUES ------------------------------------------------
+
+# Fixed values for non-tested parameters
+speed_fixed = 2
+sep_fixed = 2
+
+# Todo: figure out how to get the range we want. Must be non-negative!
+
+# Defines the distribution as a range of values. Size is # of variables (and
+# therefore runs of the model), a is the number that the distribution is based
+# around.
+vision_dist = gamma.rvs(size=100, a=10)
+
+steps = 200  # number of steps to run the model for each time
+
+burn_in = 10  # number of steps to exclude at the beginning as collective behaviour emerges
+
+
+# RUN MODELS & COLLECT DATA ---------------------------------------------------
+def run_vision_model(vision):
+    """
+    Runs the shoal model for a certain number of steps with vision radius
+    varying while all other parameters are fixed. Returns a dataframe with the
+    average per run of all data collectors (average of all steps) and columns
+    with the parameter values for that run, including the varying & fixed
+    parameters so all dataframes can be stacked together.
+    """
+    vision_parameter = []
+    model = ShoalModel(n_fish=20,
+                       width=50,
+                       height=50,
+                       speed=speed_fixed,
+                       vision=vision_dist,
+                       separation=sep_fixed)
+    for step in range(steps):
+        model.step()  # run the model for certain number of steps
+        vision_parameter.append(vision)  # create list of parameter values tested
+    data = model.datacollector.get_model_vars_dataframe()  # retrieve data from model
+    data["speed"] = speed_fixed  # add parameter value column
+    data["vision"] = vision_parameter  # add vision column
+    data["separation"] = sep_fixed  # add separation column
+    data_trim = data.iloc[burn_in:, ]  # remove early runs
+    return pd.DataFrame(data_trim.mean(axis=0)).T  # return means of all columns & transposed
+
+
+# Run the model as many times as there are parameter values
+vision_data = pd.concat([run_vision_model(i) for i in vision_dist])
+print(vision_data)  # printing makes the data accessible from the cluster.
