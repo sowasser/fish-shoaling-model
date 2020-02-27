bae8555a72689c7ce28cf284b1eafe854a19a3af
Sophia Wassermann <hi@sowasser.com>
2020-02-27 11:59:56 +0000

    Updated task creation to send outputs to a folder with today's date

--- ICHEC_files/taskfarm/create_tasks.py
+++ ICHEC_files/taskfarm/create_tasks.py
@@ -12,15 +12,15 @@ file = open(os.path.join(path, r"modelruns.txt"), "w")
 # Write a line running the scripts varying each parameter (speed, vision,
 # separation) i times
 for i in range(3):
-    file.write("python3 ../../ichec_run_speed.py > ../output/26feb2020/speed_output"
+    file.write("python3 ../../ichec_run_speed.py > ../output/27feb2020/speed_output"
                + str(i) + ".txt \n")
 
 for i in range(3):
-    file.write("python3 ../../ichec_run_vision.py > ../output/26feb2020/vision_output"
+    file.write("python3 ../../ichec_run_vision.py > ../output/27feb2020/vision_output"
                + str(i) + ".txt \n")
 
 for i in range(3):
-    file.write("python3 ../../ichec_run_sep.py > ../output/26feb2020/sep_output"
+    file.write("python3 ../../ichec_run_sep.py > ../output/27feb2020/sep_output"
                + str(i) + ".txt \n")
 
 file.close()
