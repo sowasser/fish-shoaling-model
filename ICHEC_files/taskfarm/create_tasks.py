35d5164f250b44cf04c8528b095b0b3bc313428e
Sophia Wassermann <hi@sowasser.com>
2020-03-19 12:03:47 +0000

    New output folder for today & added path for laptop

--- ICHEC_files/taskfarm/create_tasks.py
+++ ICHEC_files/taskfarm/create_tasks.py
@@ -6,21 +6,23 @@ own data output.
 
 import os
 
-path = "/Users/user/Desktop/Local/Mackerel/fish-shoaling-model/ICHEC_files/taskfarm"
+# path = "/Users/user/Desktop/Local/Mackerel/fish-shoaling-model/ICHEC_files/taskfarm"  # desktop
+path = "/Users/Sophie/Desktop/DO NOT ERASE/1NUIG/Mackerel/fish-shoaling-model/ICHEC_files/taskfarm"  # laptop
+
 file = open(os.path.join(path, r"modelruns.txt"), "w")
 
 # Write a line running the scripts varying each parameter (speed, vision,
 # separation) i times
 for i in range(10):
-    file.write("python3 ../../ichec_run_speed.py > ../output/27feb2020/speed_output"
+    file.write("python3 ../../ichec_run_speed.py > ../output/19Mar2020/speed_output"
                + str(i) + ".txt \n")
 
 for i in range(10):
-    file.write("python3 ../../ichec_run_vision.py > ../output/27feb2020/vision_output"
+    file.write("python3 ../../ichec_run_vision.py > ../output/19Mar2020/vision_output"
                + str(i) + ".txt \n")
 
 for i in range(10):
-    file.write("python3 ../../ichec_run_sep.py > ../output/27feb2020/sep_output"
+    file.write("python3 ../../ichec_run_sep.py > ../output/19Mar2020/sep_output"
                + str(i) + ".txt \n")
 
 file.close()
