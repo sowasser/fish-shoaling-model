ad89851ec9f2b931f5f93cb5e24224168b7a30ff
Sophia Wassermann <hi@sowasser.com>
2020-03-27 09:51:57 +0000

    Next set of runs, 200-299

--- ICHEC_files/taskfarm/create_tasks.py
+++ ICHEC_files/taskfarm/create_tasks.py
@@ -13,15 +13,15 @@ file = open(os.path.join(path, r"modelruns.txt"), "w")
 
 # Write a line running the scripts varying each parameter (speed, vision,
 # separation) i times
-for i in range(100, 200):
+for i in range(200, 300):
     file.write("python3 ../../ichec_run_speed.py > ../output/25Mar2020/speed_output"
                + str(i) + ".txt \n")
 
-for i in range(100, 200):
+for i in range(200, 300):
     file.write("python3 ../../ichec_run_vision.py > ../output/25Mar2020/vision_output"
                + str(i) + ".txt \n")
 
-for i in range(100, 200):
+for i in range(200, 300):
     file.write("python3 ../../ichec_run_sep.py > ../output/25Mar2020/sep_output"
                + str(i) + ".txt \n")
 
