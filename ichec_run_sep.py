e2a61880d60677b609100e95b86043b1a2b43de4
Sophia Wassermann <hi@sowasser.com>
2020-03-30 11:21:27 +0100

    Simplified the variable definition & function because it now runs once

--- ichec_run_sep.py
+++ ichec_run_sep.py
@@ -1,34 +1,20 @@
 """
 This file is for testing the effect of different parameter values on the output
 from the data collectors, when running on the ICHEC cluster. In this script,
-separation varies while speed and vision remain fixed.
+a value (prior) for separation is passed in from a defined gamma distribution
+(from create_priors.py & sep_priors.txt).
 """
 
 from shoal_model import *
 import pandas as pd
-from scipy.stats import gamma
-
-
-# SET PARAMETER & MODEL VALUES ------------------------------------------------
+import sys
 
 # Fixed values for non-tested parameters
 speed_fixed = 2
 vision_fixed = 10
 
-# Todo: figure out how to get the range we want. Must be non-negative!
 
-# Defines the distribution as a range of values. Size is # of variables (and
-# therefore runs of the model), a is the number that the distribution is based
-# around.
-sep_dist = [2]
-
-steps = 200  # number of steps to run the model for each time
-
-burn_in = 10  # number of steps to exclude at the beginning as collective behaviour emerges
-
-
-# RUN MODELS & COLLECT DATA ---------------------------------------------------
-def run_sep_model(separation):
+def run_sep_model(prior):
     """
     Runs the shoal model for a certain number of steps with separation distance
     varying while all other parameters are fixed. Returns a dataframe with the
@@ -42,23 +28,21 @@ def run_sep_model(separation):
                        height=50,
                        speed=speed_fixed,
                        vision=vision_fixed,
-                       separation=separation)
-    for step in range(steps):
+                       separation=prior)
+    for step in range(200):  # number of steps to run the model for
         model.step()  # run the model for certain number of steps
-        sep_parameter.append(separation)  # create list of parameter values tested
-    data = model.datacollector.get_model_vars_dataframe() # retrieve data from model
-    data["speed"] = speed_fixed  # add parameter value column
+    data = model.datacollector.get_model_vars_dataframe()  # retrieve data from model
+    data["speed"] = speed_fixed  # add speed value column
     data["vision"] = vision_fixed  # add vision column
-    data["separation"] = sep_parameter  # add separation column
-    data_trim = data.iloc[burn_in:, ]  # remove early runs
-    return pd.DataFrame(data_trim.mean(axis=0)).T  # return means of all columns & transposed
+    data["separation"] = prior  # add separation column
+    data_trim = data.iloc[10:, ]  # remove some # of early runs
+    return pd.DataFrame(data_trim.mean(axis=0)).T  # return means of all columns & transpose
 
 
-# Run the model as many times as there are parameter values
-sep_data = pd.concat([run_sep_model(i) for i in sep_dist])
+# Run the model with value passed in from prior called in create_tasks.py
+sep_data = run_sep_model(sys.argv[1])
 
 # Re-name columns so all data will print & index with unique values for R.
 sep_data.columns = ["polar", "nnd", "area", "cent", "speed", "vision", "sep"]
-pd.set_option("display.max_rows", None)  # display all rows
 
 print(sep_data)  # printing makes the data accessible from the cluster.
