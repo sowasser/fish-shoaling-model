62bdcb6e60a3747b46b68df683f8c08106b59c03
Sophia Wassermann <hi@sowasser.com>
2020-04-16 11:13:58 +0100

    Creating a smaller set of data for testing new plots for abc data in R

--- ICHEC_files/taskfarm/create_tasks.py
+++ ICHEC_files/taskfarm/create_tasks.py
@@ -10,16 +10,16 @@ import numpy as np
 import os
 
 # Prior distributions
-speed_dist = gamma.rvs(size=10000, a=2)
-vision_dist = gamma.rvs(size=10000, a=10)
-sep_dist = gamma.rvs(size=10000, a=2)
+speed_dist = gamma.rvs(size=100, a=2)
+vision_dist = gamma.rvs(size=100, a=10)
+sep_dist = gamma.rvs(size=100, a=2)
 
 # speed_dist = np.random.lognormal(mean=0.2, sigma=1, size=1000)
 # vision_dist = np.random.lognormal(mean=1, sigma=1, size=1000)
 # sep_dist = np.random.lognormal(mean=2, sigma=1, size=1000)
 
 # Same length as priors; for unique names for the output files
-names = range(10000)
+names = range(100)
 
 # path = "/Users/user/Desktop/Local/Mackerel/fish-shoaling-model/ICHEC_files/taskfarm"  # desktop
 path = "/Users/Sophie/Desktop/DO NOT ERASE/1NUIG/Mackerel/fish-shoaling-model/ICHEC_files/taskfarm"  # laptop
@@ -29,15 +29,15 @@ path = "/Users/Sophie/Desktop/DO NOT ERASE/1NUIG/Mackerel/fish-shoaling-model/IC
 file = open(os.path.join(path, r"modelruns.txt"), "w")
 
 [file.write("python3 ../../ichec_run_speed.py " + str(i)
-            + " > ../output/06Apr2020/speed_output" + str(j)  # TODO: make sure date is correct
+            + " > ../output/16Apr2020/speed_output" + str(j)  # TODO: make sure date is correct
             + ".txt \n") for i, j in zip(speed_dist, names)]
 
 [file.write("python3 ../../ichec_run_vision.py " + str(i)
-            + " > ../output/06Apr2020/vision_output" + str(j)  # TODO: make sure date is correct
+            + " > ../output/16Apr2020/vision_output" + str(j)  # TODO: make sure date is correct
             + ".txt \n") for i, j in zip(vision_dist, names)]
 
 [file.write("python3 ../../ichec_run_sep.py " + str(i)
-            + " > ../output/06Apr2020/sep_output" + str(j)  # TODO: make sure date is correct
+            + " > ../output/16Apr2020/sep_output" + str(j)  # TODO: make sure date is correct
             + ".txt \n") for i, j in zip(sep_dist, names)]
 
 file.close()
