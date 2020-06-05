b4d39c54c58ef8e2db76ac4585698b4f438f5d5e
Sophia Wassermann <hi@sowasser.com>
2020-06-05 16:17:02 +0100

    I had decreased the model area so it was throwing an error for speed >10
    Changed that back now and am trying the original distributions I wanted to run to start out with. Blahhhhhhhhh.

--- ichec_run_allfactors.py
+++ ichec_run_allfactors.py
@@ -24,8 +24,8 @@ def run_model(speed_prior, vision_prior, separation_prior,
     dataframes can be stacked together.
     """
     model = ShoalModel(n_fish=20,
-                       width=10,
-                       height=10,
+                       width=50,
+                       height=50,
                        speed=speed_prior,
                        vision=vision_prior,
                        separation=separation_prior,
