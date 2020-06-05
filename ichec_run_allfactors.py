df2e0f692974cbd7abb215416ddede74485d35cd
Sophia Wassermann <hi@sowasser.com>
2020-06-05 12:22:34 +0100

    Repeating 2 parameters 3 times. Don't think this is statistically sound

--- ichec_run_allfactors.py
+++ ichec_run_allfactors.py
@@ -14,8 +14,7 @@ import pandas as pd
 import sys
 
 
-def run_model(speed_prior, vision_prior, separation_prior,
-              cohere_prior, separate_prior, match_prior):
+def run_model(boid_prior, other_prior):
     """
     Runs the shoal model for a certain number of steps with all of the
     parameters are fixed. Returns a dataframe with the average per run of all
@@ -26,12 +25,12 @@ def run_model(speed_prior, vision_prior, separation_prior,
     model = ShoalModel(n_fish=20,
                        width=10,
                        height=10,
-                       speed=speed_prior,
-                       vision=vision_prior,
-                       separation=separation_prior,
-                       cohere=cohere_prior,
-                       separate=separate_prior,
-                       match=match_prior)
+                       speed=boid_prior,
+                       vision=boid_prior,
+                       separation=boid_prior,
+                       cohere=other_prior,
+                       separate=other_prior,
+                       match=other_prior)
     for step in range(300):  # number of steps to run the model for
         model.step()
     data = model.datacollector.get_model_vars_dataframe()  # retrieve data from model
@@ -42,18 +41,17 @@ def run_model(speed_prior, vision_prior, separation_prior,
     mean = data_trim.mean(axis=0)
     std = data_trim.std(axis=0)
     all_data = pd.concat([min, max, mean, std], axis=0)
-    all_data["speed"] = speed_prior  # add speed value column
-    all_data["vision"] = vision_prior  # add vision columnm
-    all_data["separation"] = separation_prior  # add separation column
-    all_data["cohere"] = cohere_prior  # add speed value column
-    all_data["separate"] = separate_prior  # add vision columnm
-    all_data["match"] = match_prior  # add separation column
+    all_data["speed"] = boid_prior  # add speed value column
+    all_data["vision"] = boid_prior  # add vision columnm
+    all_data["separation"] = boid_prior  # add separation column
+    all_data["cohere"] = other_prior  # add speed value column
+    all_data["separate"] = other_prior  # add vision columnm
+    all_data["match"] = other_prior  # add separation column
     return pd.DataFrame(all_data).T
 
 
 # Run model with prior called in create_tasks.py, as a float
-model_data = run_model(float(sys.argv[1]), float(sys.argv[1]), float(sys.argv[1]),
-                       float(sys.argv[2]), float(sys.argv[2]), float(sys.argv[2]))
+model_data = run_model(float(sys.argv[1]), float(sys.argv[2]))
 
 # Re-name columns so all data will print & index with unique values for R.
 model_data.columns = ["cent_min", "nnd_min", "polar_min", "area_min",
