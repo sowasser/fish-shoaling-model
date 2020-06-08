8d78f7f4d307dc6a5e2f7756a4db3bcdaba041b1
Sophia Wassermann <hi@sowasser.com>
2020-06-08 13:28:41 +0100

    Trialling model output with all steps for each run for timeseries graphs

--- ichec_run_alldata.py
+++ ichec_run_alldata.py
@@ -1,13 +1,15 @@
 """
 Basic script for running the model on the Irish High End Computing Cluster
-(ICHEC). On the cluster, the print data is saved in a .txt file for each job
-(a set of runs) completed. Many of these are created.
+(ICHEC). On the cluster, the print data is saved in a .txt file for each run,
+with each step in a new row. Many of these are created.
+
+In this file, all factors (speed, vision, separation, cohere, separate, match)
+are varied and no statistics are performed on the data before they are printed.
 """
-# Todo: figure out how to divide model runs for the different tasks
-# Todo: figure out how to combine data afterwards
 
 from shoal_model import *
 import pandas as pd
+import sys
 
 
 def run_model(speed_prior, vision_prior, separation_prior,
@@ -28,7 +30,7 @@ def run_model(speed_prior, vision_prior, separation_prior,
                        cohere=cohere_prior,
                        separate=separate_prior,
                        match=match_prior)
-    for step in range(30):  # number of steps to run the model for
+    for step in range(300):  # number of steps to run the model for
         model.step()
     data = model.datacollector.get_model_vars_dataframe()  # retrieve data from model
     # Add columns with the prior values for each parameter.
@@ -42,7 +44,8 @@ def run_model(speed_prior, vision_prior, separation_prior,
 
 
 # Run model with prior called in create_tasks.py, as a float
-model_data = run_model(2, 10, 2, 0.3, 0.03, 0.7)
+model_data = run_model(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]),
+                       float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
 
 # Re-name columns so all data will print & index with unique values for R.
 model_data.columns = ["polar", "nnd", "area", "cent",
