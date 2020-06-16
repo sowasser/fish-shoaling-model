bd2c1cb56293d95cf5a09aae4bf27a043d384778
Sophia Wassermann <hi@sowasser.com>
2020-06-16 16:19:09 +0100

    Updated distributions & added new parameters to data generation for SA

--- data_handling/data_sensitivity.py
+++ data_handling/data_sensitivity.py
@@ -31,39 +31,36 @@ import os
 
 
 # Paths for exporting the data
-path = "/Users/user/Desktop/Local/Mackerel/Mackerel Data"  # for desktop
+# path = "/Users/user/Desktop/Local/Mackerel/Mackerel Data"  # for desktop
 # path = "/Users/user/Desktop"  # for testing
-# path = "/Users/Sophie/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data"  # for laptop
+path = "/Users/Sophie/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data"  # for laptop
 
 # SET PARAMETER & MODEL VALUES ------------------------------------------------
 
-# Fixed values for non-tested parameters
-speed_fixed = 2
-vision_fixed = 10
-sep_fixed = 2
+runs = 100
 
-# Todo: figure out how to get the range we want. Must be non-negative!
+# Fixed values for non-tested parameters
+speed_fixed = 5
+vision_fixed = 5
+sep_fixed = 5
+cohere_fixed = 0.5
+separate_fixed = 0.5
+match_fixed = 0.5
+
 
 # Defines the distribution as a range of values. Size is # of variables (and
 # therefore runs of the model), a is the number that the distribution is based
 # around.
-speed_dist = gamma.rvs(size=1000, a=2)
-vision_dist = gamma.rvs(size=1000, a=10)
-sep_dist = gamma.rvs(size=1000, a=2)
+speed_dist = np.random.uniform(low=0, high=10, size=runs)
+vision_dist = np.random.uniform(low=0, high=10, size=runs)
+sep_dist = np.random.uniform(low=0, high=10, size=runs)
+cohere_dist = np.random.uniform(low=0, high=1, size=runs)
+separate_dist = np.random.uniform(low=0, high=1, size=runs)
+match_dist = np.random.uniform(low=0, high=1, size=runs)
 
-# Defines the distribution as a set of repeated values
-# speed_params = [0.1, 0.5, 2, 5, 8]  # values to repeat & run
-# speed_dist = np.repeat(speed_params, 10).tolist()  # repeats above list n times
-#
-# vision_params = [1, 5, 10, 15, 25]
-# vision_dist = np.repeat(vision_params, 10).tolist()
-#
-# sep_params = [0.1, 0.5, 2, 5, 8]
-# sep_dist = np.repeat(sep_params, 10).tolist()
+steps = 300  # number of steps to run the model for each time
 
-steps = 200  # number of steps to run the model for each time
-
-burn_in = 10  # number of steps to exclude at the beginning as collective behaviour emerges
+burn_in = 100  # number of steps to exclude at the beginning as collective behaviour emerges
 
 
 # RUN MODELS & COLLECT DATA ---------------------------------------------------
@@ -82,7 +79,10 @@ def run_speed_model(speed):
                        height=50,
                        speed=speed,
                        vision=vision_fixed,
-                       separation=sep_fixed)
+                       separation=sep_fixed,
+                       cohere=cohere_fixed,
+                       separate=separate_fixed,
+                       match=match_fixed)
     for step in range(steps):
         model.step()  # run the model for certain number of steps
         speed_parameter.append(speed)  # create list of parameter values tested
@@ -90,6 +90,9 @@ def run_speed_model(speed):
     data["speed"] = speed_parameter  # add parameter value column
     data["vision"] = vision_fixed  # add vision column
     data["separation"] = sep_fixed  # add separation column
+    data["cohere"] = cohere_fixed  # add cohere column
+    data["separate"] = separate_fixed  # add separate column
+    data["match"] = match_fixed  # add match column
     data_trim = data.iloc[burn_in:, ]  # remove early runs
     return pd.DataFrame(data_trim.mean(axis=0)).T
 
@@ -112,7 +115,10 @@ def run_vision_model(vision):
                        height=50,
                        speed=speed_fixed,
                        vision=vision,
-                       separation=sep_fixed)
+                       separation=sep_fixed,
+                       cohere=cohere_fixed,
+                       separate=separate_fixed,
+                       match=match_fixed)
     for step in range(steps):
         model.step()  # run the model for certain number of steps
         vision_parameter.append(vision)  # create list of parameter values tested
@@ -120,6 +126,9 @@ def run_vision_model(vision):
     data["speed"] = speed_fixed  # add parameter value column
     data["vision"] = vision_parameter  # add vision column
     data["separation"] = sep_fixed  # add separation column
+    data["cohere"] = cohere_fixed  # add cohere column
+    data["separate"] = separate_fixed  # add separate column
+    data["match"] = match_fixed  # add match column
     data_trim = data.iloc[burn_in:, ]  # remove early runs
     return pd.DataFrame(data_trim.mean(axis=0)).T  # return means of all columns & transposed
 
@@ -142,7 +151,10 @@ def run_sep_model(separation):
                        height=50,
                        speed=speed_fixed,
                        vision=vision_fixed,
-                       separation=separation)
+                       separation=separation,
+                       cohere=cohere_fixed,
+                       separate=separate_fixed,
+                       match=match_fixed)
     for step in range(steps):
         model.step()  # run the model for certain number of steps
         sep_parameter.append(separation)  # create list of parameter values tested
@@ -150,12 +162,111 @@ def run_sep_model(separation):
     data["speed"] = speed_fixed  # add parameter value column
     data["vision"] = vision_fixed  # add vision column
     data["separation"] = sep_parameter  # add separation column
+    data["cohere"] = cohere_fixed  # add cohere column
+    data["separate"] = separate_fixed  # add separate column
+    data["match"] = match_fixed  # add match column
+    data_trim = data.iloc[burn_in:, ]  # remove early runs
+    return pd.DataFrame(data_trim.mean(axis=0)).T  # return means of all columns & transposed
+
+# Run the model as many times as there are parameter values, for # of steps in "s"
+# sep_data = pd.concat([run_sep_model(s, i) for i in sep_dist])
+
+
+def run_cohere_model(cohere):
+    """
+    Runs the shoal model for a certain number of steps with coherence vector
+    varying while all other parameters are fixed. Returns a dataframe with the
+    average per run of all data collectors (average of all steps) and columns
+    with the parameter values for that run, including the varying & fixed
+    parameters so all dataframes can be stacked together.
+    """
+    cohere_parameter = []
+    model = ShoalModel(n_fish=20,
+                       width=50,
+                       height=50,
+                       speed=speed_fixed,
+                       vision=vision_fixed,
+                       separation=sep_fixed,
+                       cohere=cohere,
+                       separate=separate_fixed,
+                       match=match_fixed)
+    for step in range(steps):
+        model.step()  # run the model for certain number of steps
+        cohere_parameter.append(cohere)  # create list of parameter values tested
+    data = model.datacollector.get_model_vars_dataframe() # retrieve data from model
+    data["speed"] = speed_fixed  # add parameter value column
+    data["vision"] = vision_fixed  # add vision column
+    data["separation"] = sep_fixed  # add separation column
+    data["cohere"] = cohere_parameter  # add cohere column
+    data["separate"] = separate_fixed  # add separate column
+    data["match"] = match_fixed  # add match column
     data_trim = data.iloc[burn_in:, ]  # remove early runs
     return pd.DataFrame(data_trim.mean(axis=0)).T  # return means of all columns & transposed
 
 
-# Run the model as many times as there are parameter values, for # of steps in "s"
-# sep_data = pd.concat([run_sep_model(s, i) for i in sep_dist])
+def run_separate_model(separate):
+    """
+    Runs the shoal model for a certain number of steps with separate vector
+    varying while all other parameters are fixed. Returns a dataframe with the
+    average per run of all data collectors (average of all steps) and columns
+    with the parameter values for that run, including the varying & fixed
+    parameters so all dataframes can be stacked together.
+    """
+    separate_parameter = []
+    model = ShoalModel(n_fish=20,
+                       width=50,
+                       height=50,
+                       speed=speed_fixed,
+                       vision=vision_fixed,
+                       separation=sep_fixed,
+                       cohere=cohere_fixed,
+                       separate=separate,
+                       match=match_fixed)
+    for step in range(steps):
+        model.step()  # run the model for certain number of steps
+        separate_parameter.append(separate)  # create list of parameter values tested
+    data = model.datacollector.get_model_vars_dataframe() # retrieve data from model
+    data["speed"] = speed_fixed  # add parameter value column
+    data["vision"] = vision_fixed  # add vision column
+    data["separation"] = sep_fixed  # add separation column
+    data["cohere"] = cohere_fixed  # add cohere column
+    data["separate"] = separate_parameter  # add separate column
+    data["match"] = match_fixed  # add match column
+    data_trim = data.iloc[burn_in:, ]  # remove early runs
+    return pd.DataFrame(data_trim.mean(axis=0)).T  # return means of all columns & transposed
+
+
+def run_match_model(match):
+    """
+    Runs the shoal model for a certain number of steps with separate vector
+    varying while all other parameters are fixed. Returns a dataframe with the
+    average per run of all data collectors (average of all steps) and columns
+    with the parameter values for that run, including the varying & fixed
+    parameters so all dataframes can be stacked together.
+    """
+    match_parameter = []
+    model = ShoalModel(n_fish=20,
+                       width=50,
+                       height=50,
+                       speed=speed_fixed,
+                       vision=vision_fixed,
+                       separation=sep_fixed,
+                       cohere=cohere_fixed,
+                       separate=separate_fixed,
+                       match=match)
+    for step in range(steps):
+        model.step()  # run the model for certain number of steps
+        match_parameter.append(match)  # create list of parameter values tested
+    data = model.datacollector.get_model_vars_dataframe() # retrieve data from model
+    data["speed"] = speed_fixed  # add parameter value column
+    data["vision"] = vision_fixed  # add vision column
+    data["separation"] = sep_fixed  # add separation column
+    data["cohere"] = cohere_fixed  # add cohere column
+    data["separate"] = separate_fixed  # add separate column
+    data["match"] = match  # add match column
+    data_trim = data.iloc[burn_in:, ]  # remove early runs
+    return pd.DataFrame(data_trim.mean(axis=0)).T  # return means of all columns & transposed
+
 
 # MULTIPROCESSING -------------------------------------------------------------
 # Runs the model for as many times as is in the distribution of values above,
@@ -169,10 +280,16 @@ if __name__ == '__main__':
     speed_data = p.map(run_speed_model, [i for i in speed_dist])
     vision_data = p.map(run_vision_model, [i for i in vision_dist])
     sep_data = p.map(run_sep_model, [i for i in sep_dist])
+    cohere_data = p.map(run_cohere_model, [i for i in cohere_dist])
+    separate_data = p.map(run_separate_model, [i for i in separate_dist])
+    match_data = p.map(run_match_model, [i for i in match_dist])
     p.close()
     speed_data = pd.concat(speed_data)  # change into data format for export
     vision_data = pd.concat(vision_data)
     sep_data = pd.concat(sep_data)
+    cohere_data = pd.concat(cohere_data)
+    separate_data = pd.concat(separate_data)
+    match_data = pd.concat(match_data)
     print("Time taken = {} minutes".format((time.time() - start)/60))  # print how long it took
 
 # EXPORT DATA -----------------------------------------------------------------
@@ -180,3 +297,6 @@ if __name__ == '__main__':
 speed_data.to_csv(os.path.join(path, r"var-speed.csv"), index=False)
 vision_data.to_csv(os.path.join(path, r"var-vision.csv"), index=False)
 sep_data.to_csv(os.path.join(path, r"var-sep.csv"), index=False)
+cohere_data.to_csv(os.path.join(path, r"var-cohere.csv"), index=False)
+separate_data.to_csv(os.path.join(path, r"var-separate.csv"), index=False)
+match_data.to_csv(os.path.join(path, r"var-match.csv"), index=False)
