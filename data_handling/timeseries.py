98a3291c75605a086f9accf66cee7279b47ed99d
Sophia Wassermann <hi@sowasser.com>
2020-12-23 17:49:39 +0000

    Updated overlap values for new set of modelled data

--- data_handling/timeseries.py
+++ data_handling/timeseries.py
@@ -36,8 +36,8 @@ def find_overlap(min_ls, max_ls, check_ls):
 
 
 nnd_percent = find_overlap(g_nnd["min"], g_nnd["max"], tracked["nnd"])  # 0
-cent_percent = find_overlap(g_cent["min"], g_cent["max"], tracked["cent"])  # 69.697%
-polar_percent = find_overlap(g_polar["min"], g_polar["max"], tracked["polar"])  # 27.273%
+cent_percent = find_overlap(g_cent["min"], g_cent["max"], tracked["cent"])  # 60.606%
+polar_percent = find_overlap(g_polar["min"], g_polar["max"], tracked["polar"])  # 39.394
 area_percent = find_overlap(g_area["min"], g_area["max"], tracked["area"])  # 0
 
 nnd_only_percent = find_overlap(nnd_only["min"], nnd_only["max"], tracked["nnd"])  # 94.945%
