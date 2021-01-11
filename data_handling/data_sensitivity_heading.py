15aca3435509efb6e6bdaf47a92a6e13ea73214a
Sophia Wassermann <hi@sowasser.com>
2021-01-11 13:27:33 +0000

    Removed extra stuff not needed for this version & updated documentation

--- data_handling/data_sensitivity_heading.py
+++ data_handling/data_sensitivity_heading.py
@@ -3,14 +3,11 @@ Code for exporting the heading of each agent for each step of a model run to
 represent the tilt angle of the fish, when the model is viewed side-on / as a
 cross-section of the water column.
 
-The data is collected, when running the Shoal Model, with the functions
-outlined in data_collectors.py. Here, they are separated and manipulated to
-create more simple outputs
+The data is collected, when running the Shoal Model, with the heading function
+outlined in data_collectors.py, which produces heading/tilt angle in radians.
+Here, the data structure is simplified to create an output that can be easily
+read into R.
 """
-
-# Todo: try to create animated heatmaps (or, rather, pcolormesh) for density:
-# https://www.kaggle.com/jaeyoonpark/heatmap-animation-us-drought-map/code
-# https://www.programcreek.com/python/example/102329/matplotlib.pyplot.pcolormesh
 
 from shoal_model_heading import *
 import pandas as pd
@@ -21,7 +18,7 @@ from matplotlib import animation
 # path = "/Users/user/Desktop/Local/Mackerel/Mackerel Data"
 path = "/Users/Sophie/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data"  # for laptop
 
-n = 30  # number of fish
+n = 300  # number of fish
 
 # Collect the data from a single run with x number of steps into a dataframe
 model = ShoalModel(n_fish=n,
@@ -33,7 +30,7 @@ model = ShoalModel(n_fish=n,
                    cohere=0.25,
                    separate=0.025,
                    match=0.3)
-for i in range(4):
+for i in range(400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 
@@ -53,4 +50,4 @@ list_fish = ["fish" + str(i) for i in range(1, (n + 1))]
 head_df.columns = list_fish
 
 # Export to .csv for import into R
-head.to_csv(os.path.join(path, r"headings_300.csv"))
+head_df.to_csv(os.path.join(path, r"headings_300.csv"))
